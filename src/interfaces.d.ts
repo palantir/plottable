75fd339082d24fe1631246edf841aec490bfa461
Daniel Mane <danmane@gmail.com>
2014-01-27 10:15:59 -0800

    Some interface stuff

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -1,45 +1,22 @@
 interface IRenderable {
-  element: D3.Selection; // kind of vestigial
-  render: (element: D3.Selection, availableWidth: number, availableHeight: number) => void;
-  getRequestedWidth: (availableWidth: number, availableHeight: number) => number;
-  getRequestedHeight: (availableWidth: number, availableHeight: number) => number;
+  render: (element: D3.Selection, width: number, height: number) => ();
+
+  rowWeight: (newVal?: number) => number;
+  colWeight: (newVal?: number) => number;
+  rowMinimum: () => number;
+  colMinimum: () => number;
+
 }
 
 interface IDatum {
 
 }
 
-interface XYDatum {
-  x: number;
-  y: number;
+interface IRenderer<T extends IDatum> implements IRendererable {
+
 }
 
-interface TimeSeriesDatum extends XYDatum {
-  x: Date;
-  y: number;
-}
+interface IRendererDraggable implements IRenderer {
+  // how to reference the generic <T>? is there a need to?
 
-// interface DateYDatum extends IDatum {
-//   x: Date;
-//   y: number;
-// }
-
-interface IDataset {
-  data: IDatum[];
-  seriesName: string;
-}
-
-interface IWeatherDatum {
-  avg   : number; // Average temperature on date
-  avgh  : number;
-  avgl  : number;
-  hi    : number;
-  hih   : number;
-  hil   : number;
-  lo    : number;
-  loh   : number;
-  lol   : number;
-  precip: number;
-  day   : number;
-  date  : Date;
 }
