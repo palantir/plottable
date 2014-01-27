94920277a815603cf6c179b5c948883b7e4ffa0f
Daniel Mane <danmane@gmail.com>
2014-01-27 15:03:37 -0800

    Demo works for the multi-chart

--- src/demo.ts
+++ src/demo.ts
@@ -13,14 +13,29 @@ function makeRandomData(numPoints): IDataset {
   return {"data": data, "seriesName": "random-data"};
 }
 
+function makeBasicChartTable() {
+  var xScale = d3.scale.linear();
+  var xAxis = new XAxis(xScale, "bottom");
+  var yScale = d3.scale.linear();
+  var yAxis = new YAxis(yScale, "right");
+  var data = makeRandomData(30);
+  var renderArea = new LineRenderer(data, xScale, yScale);
+  var rootTable = new Table([[renderArea, yAxis], [xAxis, null]])
+  return rootTable;
+}
+
 // make a regular table with 1 axis on bottom, 1 axis on left, renderer in center
-var xScale = d3.scale.linear();
-var xAxis = new XAxis(xScale, "bottom");
-var yScale = d3.scale.linear();
-var yAxis = new YAxis(yScale, "right");
-var data = makeRandomData(100);
-var renderArea = new LineRenderer(data, xScale, yScale);
-var rootTable = new Table([[renderArea, yAxis], [xAxis, null]])
 
 var svg1 = d3.select("#svg1");
-rootTable.render(svg1, 500, 500);
+makeBasicChartTable().render(svg1, 500, 500);
+
+var svg2 = d3.select("#svg2");
+
+var t1 = makeBasicChartTable();
+var t2 = makeBasicChartTable();
+var t3 = makeBasicChartTable();
+var t4 = makeBasicChartTable();
+
+var metaTable = new Table([[t1, t2], [t3, t4]]);
+metaTable.render(svg2, 800, 800);
+// make a table with four nested tables
