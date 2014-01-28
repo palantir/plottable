fccc4634ebf7bb28908465f0edb99e43ca1013d9
Justin Lan <jlan@palantir.com>
2014-01-27 17:09:52 -0800

    Added test/ to Grunt tasks. Rudimentary test file for confirmation.

---
+++ test/axisTests.ts
@@ -0,0 +1,14 @@
+///<reference path="../lib/chai/chai.d.ts" />
+///<reference path="../lib/chai/chai-assert.d.ts" />
+///<reference path="../lib/mocha/mocha.d.ts" />
+
+///<reference path="../src/interfaces.d.ts" />
+///<reference path="../src/axis.ts" />
+
+var assert = chai.assert;
+
+describe("truthiness", () => {
+  it("true", () => {
+    assert.isTrue(true, "true is true!");
+  })
+})
