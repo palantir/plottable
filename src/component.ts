2af1503359fca788caccef860b52bbbe2bae310b
Daniel Mane <danmane@gmail.com>
2014-01-30 13:06:22 -0800

    Remove anchored and laid out flags

--- src/component.ts
+++ src/component.ts
@@ -3,9 +3,6 @@ class Component {
   public hitBox: D3.Selection;
   public boundingBox: D3.Selection;
 
-  private anchored = false;
-  private laidOut = false;
-
   private rowWeightVal  = 0;
   private colWeightVal  = 0;
   private rowMinimumVal = 0;
@@ -20,21 +17,18 @@ class Component {
     this.element = element;
     this.hitBox = element.append("rect").classed("hit-box", true);
     this.boundingBox = element.append("rect").classed("bounding-box", true);
-    this.anchored = true;
   }
 
   public computeLayout(xOffset?: number, yOffset?: number, availableWidth?: number, availableHeight?: number) {
     if (xOffset == null || yOffset == null || availableWidth == null || availableHeight == null) {
-      if (this.laidOut) {
-        return; // idempotent if called after initial layout
-      } else if (this.anchored && this.element.node().nodeName === "svg") {
+      if (this.element == null) {
+        throw new Error("It's impossible to computeLayout before anchoring");
+      } else if (this.element.node().nodeName === "svg") {
         // we are the root node, let's guess width and height for convenience
         xOffset = 0;
         yOffset = 0;
         availableWidth  = parseFloat(this.element.attr("width" ));
         availableHeight = parseFloat(this.element.attr("height"));
-      } else if (!this.anchored) {
-        throw new Error("It's impossible to computeLayout before anchoring");
       } else {
         throw new Error("You need to pass non-null arguments when calling computeLayout on a non-root node");
       }
@@ -46,13 +40,10 @@ class Component {
     this.element.attr("transform", "translate(" + this.xOffset + "," + this.yOffset + ")");
     this.hitBox.attr("width", this.availableWidth).attr("height", this.availableHeight);
     this.boundingBox.attr("width", this.availableWidth).attr("height", this.availableHeight);
-    this.laidOut = true;
   }
 
   public render() {
-    if (!this.laidOut) {
-      this.computeLayout()
-    }
+    //no-op
   }
 
   public zoom(translate, scale) {
@@ -65,7 +56,6 @@ class Component {
     if (newVal != null) {
       this.rowWeightVal = newVal;
       chai.assert.operator(this.rowWeightVal, '>=', 0, "rowWeight is a reasonable number");
-      this.laidOut = false;
       return this;
     } else {
       return this.rowWeightVal;
@@ -78,7 +68,6 @@ class Component {
     if (newVal != null) {
       this.colWeightVal = newVal;
       chai.assert.operator(this.colWeightVal, '>=', 0, "colWeight is a reasonable number");
-      this.laidOut = false;
       return this;
     } else {
       return this.colWeightVal
@@ -91,7 +80,6 @@ class Component {
     if (newVal != null) {
       this.rowMinimumVal = newVal;
       chai.assert.operator(this.rowMinimumVal, '>=', 0, "rowMinimum is a reasonable number");
-      this.laidOut = false;
       return this;
     } else {
       return this.rowMinimumVal;
@@ -104,7 +92,6 @@ class Component {
     if (newVal != null) {
       this.colMinimumVal = newVal;
       chai.assert.operator(this.colMinimumVal, '>=', 0, "colMinimum is a reasonable number");
-      this.laidOut = false;
       return this;
     } else {
       return this.colMinimumVal;
