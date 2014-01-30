6ed7a5d747456a06fdb918a642afe43424833f12
Daniel Mane <danmane@gmail.com>
2014-01-29 19:28:24 -0800

    Add error handling to layout code

--- src/component.ts
+++ src/component.ts
@@ -1,6 +1,10 @@
 class Component {
   public element: D3.Selection;
   public hitBox: D3.Selection;
+
+  private anchored = false;
+  private layedOut = false;
+
   public zoom(translate, scale) {
     //noop - hackhack to appease TSC
   }
@@ -9,27 +13,46 @@ class Component {
   private rowMinimumVal = 0;
   private colMinimumVal = 0;
 
-  public availableWidth = 0;
-  public availableHeight = 0;
+  public availableWidth : number;
+  public availableHeight: number;
+  private xOffset       : number;
+  private yOffset       : number;
 
   public anchor(element: D3.Selection) {
     this.element = element;
     this.hitBox = element.append("rect").classed("hit-box", true);
+    this.anchored = true;
   }
 
-  public computeLayout(xOffset = 0, yOffset = 0, availableWidth: number = null, availableHeight: number = null) {
-    if (availableWidth == null || availableHeight == null) {
-      availableWidth  = parseFloat(this.element.attr("width" ));
-      availableHeight = parseFloat(this.element.attr("height"));
+  public computeLayout(xOffset?: number, yOffset?: number, availableWidth?: number, availableHeight?: number) {
+    if (xOffset == null || yOffset == null || availableWidth == null || availableHeight == null) {
+      if (this.layedOut) {
+        return; // idempotent if called after initial layout
+      } else if (this.anchored && this.element.node().nodeName === "svg") {
+        // we are the root node, let's guess width and height for convenience
+        xOffset = 0;
+        yOffset = 0;
+        availableWidth  = parseFloat(this.element.attr("width" ));
+        availableHeight = parseFloat(this.element.attr("height"));
+      } else if (!this.anchored) {
+        throw new Error("It's impossible to computeLayout before anchoring");
+      } else {
+        throw new Error("You need to pass non-null arguments when calling computeLayout on a non-root node");
+      }
     }
-    this.element.attr("transform", "translate(" + xOffset + "," + yOffset + ")");
-    this.hitBox.attr("width", availableWidth).attr("height", availableHeight);
+    this.xOffset = xOffset;
+    this.yOffset = yOffset;
     this.availableWidth = availableWidth;
     this.availableHeight = availableHeight;
+    this.element.attr("transform", "translate(" + this.xOffset + "," + this.yOffset + ")");
+    this.hitBox.attr("width", this.availableWidth).attr("height", this.availableHeight);
+    this.layedOut = true;
   }
 
   public render() {
-    // no-op
+    if (!this.layedOut) {
+      this.computeLayout()
+    }
   }
 
   public rowWeight(): number;
@@ -38,6 +61,7 @@ class Component {
     if (newVal != null) {
       this.rowWeightVal = newVal;
       chai.assert.operator(this.rowWeightVal, '>=', 0, "rowWeight is a reasonable number");
+      this.layedOut = false;
       return this;
     } else {
       return this.rowWeightVal;
@@ -50,6 +74,7 @@ class Component {
     if (newVal != null) {
       this.colWeightVal = newVal;
       chai.assert.operator(this.colWeightVal, '>=', 0, "colWeight is a reasonable number");
+      this.layedOut = false;
       return this;
     } else {
       return this.colWeightVal
@@ -62,6 +87,7 @@ class Component {
     if (newVal != null) {
       this.rowMinimumVal = newVal;
       chai.assert.operator(this.rowMinimumVal, '>=', 0, "rowMinimum is a reasonable number");
+      this.layedOut = false;
       return this;
     } else {
       return this.rowMinimumVal;
@@ -74,6 +100,7 @@ class Component {
     if (newVal != null) {
       this.colMinimumVal = newVal;
       chai.assert.operator(this.colMinimumVal, '>=', 0, "colMinimum is a reasonable number");
+      this.layedOut = false;
       return this;
     } else {
       return this.colMinimumVal;
