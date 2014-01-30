fe611028d50a8de424ac9e110c39b9718995df33
Daniel Mane <danmane@gmail.com>
2014-01-30 14:27:15 -0800

    Axes now rescale properly on zoom in zoom interaction

--- src/interaction.ts
+++ src/interaction.ts
@@ -2,9 +2,11 @@
 
 class DragZoomInteraction {
   private zoom;
-  constructor(public elementToListenTo: D3.Selection, public renderers: Component[]) {
+  constructor(public elementToListenTo: D3.Selection, public renderers: Component[], public xScale: Scale, public yScale: Scale) {
     this.zoom = d3.behavior.zoom();
     this.zoom(elementToListenTo);
+    this.zoom.x(this.xScale.scale);
+    this.zoom.y(this.yScale.scale);
     var throttledZoom = _.throttle(() => this.rerenderZoomed(), 30);
     this.zoom.on("zoom", throttledZoom);
   }
