b8977a075a849753608b4bd9e961f51987e36acf
Daniel Mane <danmane@gmail.com>
2014-01-30 18:06:14 -0800

    Prototyped AreaInteraction code

--- src/interaction.ts
+++ src/interaction.ts
@@ -49,7 +49,10 @@ interface XYSelectionArea {
 }
 
 class AreaInteraction extends Interaction {
-  private drag;
+  private static CLASS_DRAG_BOX = "drag-box";
+  private dragInitialized = false;
+  private dragBehavior;
+  private origin = [0,0];
   private dragBox: D3.Selection;
 
   constructor(
@@ -57,16 +60,43 @@ class AreaInteraction extends Interaction {
     private areaCallback: (a: XYSelectionArea) => any
   ) {
     super(rendererComponent);
-    this.drag = d3.behavior.drag();
-    this.drag.on("dragstart", () => console.log("dragstart"));
-    this.drag.on("drag", () => console.log("drag"));
-    this.drag.on("dragend", () => console.log("dragend"));
+    this.dragBehavior = d3.behavior.drag();
+    this.dragBehavior.on("dragstart", () => this.dragstart());
+    this.dragBehavior.on("drag", () => this.drag());
+    this.dragBehavior.on("dragend", () => this.dragend());
     this.registerWithComponent();
   }
 
 
 
+  private dragstart(){
+    var x = d3.event.x;
+    var y = d3.event.y;
+    this.dragBox.attr("height", 0).attr("width", 0);
+  }
+
+  private drag(){
+    if (!this.dragInitialized) {
+      this.origin = [d3.event.x, d3.event.y];
+      this.dragInitialized = true;
+    }
+    var location = [d3.event.x, d3.event.y];
+    var width  = Math.abs(this.origin[0] - location[0]);
+    var height = Math.abs(this.origin[1] - location[1]);
+    var x = Math.min(this.origin[0], location[0]);
+    var y = Math.min(this.origin[1], location[1]);
+    this.dragBox.attr("x", x).attr("y", y).attr("height", height).attr("width", width);
+  }
+
+  private dragend(){
+    this.dragInitialized = false;
+    // this.dragBox.attr("height", 0).attr("width", 0);
+  }
+
   public listenToHitBox(hitBox: D3.Selection) {
-    hitBox.call(this.drag);
+    var cname = AreaInteraction.CLASS_DRAG_BOX;
+    var element = this.componentToListenTo.element;
+    this.dragBox = element.append("rect").classed(cname, true).attr("x", 0).attr("y", 0);
+    hitBox.call(this.dragBehavior);
   }
 }
