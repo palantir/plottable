126f191f6a508cc9aa259784d307e5e2406661d8
Daniel Mane <danmane@gmail.com>
2014-02-01 00:59:42 -0800

    Add coordinators to the sparkline demo

--- examples/sparklineDemo.ts
+++ examples/sparklineDemo.ts
@@ -26,6 +26,11 @@ var r3: Component[] = [null, sparkline];
 var chart = new Table([r1, r2, r3]);
 chart.xMargin = 10;
 chart.yMargin = 10;
+
+var brushZoom = new BrushZoomInteraction(sparkline, xScale, ySpark);
+var zoomCoordinator = new ScaleDomainCoordinator([yScale, ySpark]);
+
+
 var svg = d3.select("#table");
 chart.anchor(svg);
 chart.computeLayout();
