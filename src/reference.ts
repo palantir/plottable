ac248736e98cc54c60a80aa701aee7b3fabd52e4
Daniel Mane <danmane@gmail.com>
2014-02-05 13:25:57 -0800

    Update Gruntfile.js to use grunt-ts. More intelligent compliation, don't recompile the whole project if we only modified a test. Generate plottable.d.ts for reference from the test and example files rather than direct dependency. Generate reference.ts for smarter dependency management within the core project.

---
+++ src/reference.ts
@@ -0,0 +1,12 @@
+/// <reference path="utils.ts" />
+/// <reference path="component.ts" />
+/// <reference path="scale.ts" />
+//grunt-start
+/// <reference path="axis.ts" />
+/// <reference path="interaction.ts" />
+/// <reference path="interfaces.d.ts" />
+/// <reference path="labelComponent.ts" />
+/// <reference path="perfdiagnostics.ts" />
+/// <reference path="renderer.ts" />
+/// <reference path="table.ts" />
+//grunt-end
