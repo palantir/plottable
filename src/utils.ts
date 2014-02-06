5029f0e6f44e99eead3cf123a46134cc20a367f3
Daniel Mane <danmane@gmail.com>
2014-02-05 19:59:59 -0800

    Make spacing in utils consistent (spaces not tabs)

--- src/utils.ts
+++ src/utils.ts
@@ -1,29 +1,29 @@
 ///<reference path="reference.ts" />
 
 module Utils {
-	export function readyCallback(numToTrigger: number, callbackWhenReady: () => any) {
-		var timesCalled = 0;
-		return () => {
-		     timesCalled++;
-		  if (timesCalled === numToTrigger) {
-		    callbackWhenReady();
-		  }
-		}
-	}
+  export function readyCallback(numToTrigger: number, callbackWhenReady: () => any) {
+    var timesCalled = 0;
+    return () => {
+         timesCalled++;
+      if (timesCalled === numToTrigger) {
+        callbackWhenReady();
+      }
+    }
+  }
 
-	export function translate(element: D3.Selection, translatePair: number[]) {
-		return element.attr("transform", "translate(" + translatePair + ")");
-	}
+  export function translate(element: D3.Selection, translatePair: number[]) {
+    return element.attr("transform", "translate(" + translatePair + ")");
+  }
 
-	export function getTranslate(element: D3.Selection) {
-		return d3.transform(element.attr("transform")).translate;
-	}
-	export function getBBox(element: D3.Selection): SVGRect {
-		return (<any> element.node()).getBBox();
-	}
-	export function setWidthHeight(elements: D3.Selection[], width: number, height: number) {
-		elements.forEach((e) => {
-			e.attr("width", width).attr("height", height);
-			})
-	}
+  export function getTranslate(element: D3.Selection) {
+    return d3.transform(element.attr("transform")).translate;
+  }
+  export function getBBox(element: D3.Selection): SVGRect {
+    return (<any> element.node()).getBBox();
+  }
+  export function setWidthHeight(elements: D3.Selection[], width: number, height: number) {
+    elements.forEach((e) => {
+      e.attr("width", width).attr("height", height);
+      })
+  }
 }
