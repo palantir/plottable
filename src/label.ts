cd6ea6b0248046a637f68edf419ca3a046016ff1
Justin Lan <jlan@palantir.com>
2014-02-11 14:19:11 -0800

    Label now truncates; fixed label tests.
    Col/Row minimum is no longer set for horizontal/veritical labels.
    If the text is too long, the label is truncated intelligently and ellipses
    are appended.
    Italic text is padded a bit to compensate for the tilted letters.
    
    Close #84. Close #89.

--- src/label.ts
+++ src/label.ts
@@ -7,8 +7,11 @@ class Label extends Component {
   public yAlignment = "CENTER";
 
   private textElement: D3.Selection;
-  private text: string;
+  private text: string; // text assigned to the element; may not be the actual text displaed due to truncation
+  private displayText: string;
   private orientation: string;
+  private textLength: number;
+  private textHeight: number;
 
   constructor(text = "", orientation = "horizontal") {
     super();
@@ -36,23 +39,95 @@ class Label extends Component {
 
   private setMinimumsByCalculatingTextSize() {
     this.textElement.attr("dy", 0); // Reset this so we maintain idempotence
-    var bbox = Utils.getBBox(this.element);
+    var bbox = Utils.getBBox(this.textElement);
     this.textElement.attr("dy", -bbox.y);
-    var clientHeight = bbox.height;
-    var clientWidth  = bbox.width;
+    this.textHeight = bbox.height;
+    this.textLength = bbox.width;
+
+    // italic text needs a slightly larger bounding box
+    if (this.textElement.style("font-style") === "italic") {
+      var textNode = <SVGTextElement> this.textElement.node();
+      // pad by half the width of the last character
+      this.textLength += 0.5 * textNode.getExtentOfChar(textNode.textContent.length-1).width;
+    }
 
     if (this.orientation === "horizontal") {
-      this.rowMinimum(clientHeight);
-      this.colMinimum(clientWidth);
+      this.rowMinimum(this.textHeight);
     } else {
-      this.colMinimum(clientHeight);
-      this.rowMinimum(clientWidth);
-      if (this.orientation === "vertical-right") {
-        this.textElement.attr("transform", "rotate(90)").attr("y", -clientHeight);
-      } else if (this.orientation === "vertical-left") {
-        this.textElement.attr("transform", "rotate(-90)").attr("x", -clientWidth);
+      this.colMinimum(this.textHeight);
+    }
+  }
+
+  private truncateTextToLength(availableLength: number) {
+    this.textElement.text(this.text + "...");
+    var textNode = <SVGTextElement> this.textElement.node();
+    var numChars = textNode.textContent.length;
+    var dotLength = textNode.getSubStringLength(numChars-3, 3);
+    for (var i=0; i<numChars; i++) {
+      var testLength = textNode.getSubStringLength(0, i);
+      if ((testLength + dotLength) > availableLength) {
+        if (i > 0) {
+          this.textElement.text(this.text.substr(0, i-1).trim() + "...");
+        } else {
+          this.textElement.text(""); // no room even for ellipsis
+        }
+        this.setMinimumsByCalculatingTextSize();
+        return;
       }
     }
+  }
+
+  public computeLayout(xOffset?: number, yOffset?: number, availableWidth?: number, availableHeight?: number) {
+    super.computeLayout(xOffset, yOffset, availableWidth, availableHeight);
+
+    var xShift = 0;
+    var yShift = 0;
+
+    if (this.orientation === "horizontal") {
+      if (this.availableWidth < this.textLength) {
+        this.truncateTextToLength(this.availableWidth);
+      }
+      switch (this.xAlignment) {
+        case "LEFT":
+          break;
+        case "CENTER":
+          xShift = (this.availableWidth - this.textLength) / 2;
+          break;
+        case "RIGHT":
+          xShift = this.availableWidth - this.textLength;
+          break;
+        default:
+          throw this.xAlignment + " is not a supported alignment";
+      }
+    } else {
+      if (this.availableHeight < this.textLength) {
+        this.truncateTextToLength(this.availableHeight);
+      }
+      switch (this.yAlignment) {
+        case "TOP":
+          break;
+        case "CENTER":
+          xShift = (this.availableHeight - this.textLength) / 2;
+          break;
+        case "BOTTOM":
+          xShift = this.availableHeight - this.textLength;
+          break;
+        default:
+          throw this.yAlignment + " is not a supported alignment";
+      }
+
+      if (this.orientation === "vertical-right") {
+        this.textElement.attr("transform", "rotate(90)");
+        yShift = -this.textHeight;
+      } else { // vertical-left
+        this.textElement.attr("transform", "rotate(-90)");
+        xShift = -xShift - this.textLength; // flip xShift
+      }
+    }
+
+    this.textElement.attr("x", xShift);
+    this.textElement.attr("y", yShift);
+    return this;
   }
 }
 
