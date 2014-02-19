6457f0ee3890b05c18bfba18873cb7a1c42e8920
Daniel Mane <danmane@gmail.com>
2014-02-19 12:41:08 -0800

    Add componentGroups, which allow multiple components to overlap. Add unit testing for componentGroups. Had to make some minor changes to component.ts (make xOffset, yOffset public not private). Added some helper functions to testUtils.

--- src/component.ts
+++ src/component.ts
@@ -15,8 +15,8 @@ class Component {
 
   public availableWidth : number;
   public availableHeight: number;
-  private xOffset       : number;
-  private yOffset       : number;
+  public xOffset       : number;
+  public yOffset       : number;
 
   private cssClasses: string[] = ["component"];
 
