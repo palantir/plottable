e5c8cece3d8d80555cb4ebf45ddc853c3a5a2d3b
Justin Lan <jlan@palantir.com>
2014-02-21 17:18:07 -0800

    Change demo around so script gets executed at the right time
    Wrapped the code in a function, then called it after the SVG is
    added to the DOM.

--- examples/simpleChart/demo.js
+++ examples/simpleChart/demo.js
@@ -1,20 +1,22 @@
-function makeQuadraticSeries(numPoints) {
-    var data = [];
-    for (var i = 0; i< numPoints; i++) {
-        data.push({x: i, y: i*i});
-    }
-    return {data: data, seriesName: "quadratic-series"};
+function createSimpleChart() {
+  function makeQuadraticSeries(numPoints) {
+      var data = [];
+      for (var i = 0; i< numPoints; i++) {
+          data.push({x: i, y: i*i});
+      }
+      return {data: data, seriesName: "quadratic-series"};
+  }
+
+  var dataseries = makeQuadraticSeries(20);
+
+  var svg = d3.select("#chart");
+  svg.attr("width", 480).attr("height", 320);
+  var xScale = new LinearScale();
+  var yScale = new LinearScale();
+  var xAxis = new XAxis(xScale, "bottom");
+  var yAxis = new YAxis(yScale, "left");
+  var renderAreaD1 = new CircleRenderer(dataseries, xScale, yScale);
+  var basicTable = new Table([[yAxis, renderAreaD1],
+                              [null, xAxis]]);
+  basicTable.anchor(svg).computeLayout().render();
 }
-
-var dataseries = makeQuadraticSeries(20);
-
-var svg = d3.select("#chart");
-svg.attr("width", 480).attr("height", 320);
-var xScale = new LinearScale();
-var yScale = new LinearScale();
-var xAxis = new XAxis(xScale, "bottom");
-var yAxis = new YAxis(yScale, "left");
-var renderAreaD1 = new CircleRenderer(dataseries, xScale, yScale);
-var basicTable = new Table([[yAxis, renderAreaD1],
-                            [null, xAxis]]);
-basicTable.anchor(svg).computeLayout().render();
