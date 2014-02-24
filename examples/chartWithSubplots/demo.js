13afbb81544c33cfe501bb6af865adc52b7619c8
Justin Lan <jlan@palantir.com>
2014-02-24 12:51:03 -0800

    Fix "Chart with Subplots" demo

--- examples/chartWithSubplots/demo.js
+++ examples/chartWithSubplots/demo.js
@@ -11,10 +11,10 @@ window.onload = function() {
   var yScale2 = new LinearScale();
   var yAxis2 = new YAxis(yScale2, "right");
 
-  var renderAreaD1 = new CircleRenderer(dataseries1, xScale, yScale1).classed("series-1");
-  var renderAreaD2 = new CircleRenderer(dataseries2, xScale, yScale2).classed("series-2");
+  var renderAreaD1 = new CircleRenderer(dataseries1, xScale, yScale1).classed("series-1", true);
+  var renderAreaD2 = new CircleRenderer(dataseries2, xScale, yScale2).classed("series-2", true);
 
-  var basicTable = new Table([[yAxis, renderAreaD1, null],
+  var basicTable = new Table([[yAxis1, renderAreaD1, null],
                               [null,  renderAreaD2, yAxis2],
                               [null,  xAxis,        null]]);
   basicTable.anchor(svg).computeLayout().render();
