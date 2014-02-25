c8dd6cdb56961f95c712c5286de4643983638c33
Daniel Mane <danmane@gmail.com>
2014-02-25 11:54:03 -0800

    ComponentGroup has chainable addComponent call

--- src/componentGroup.ts
+++ src/componentGroup.ts
@@ -3,18 +3,29 @@
 class ComponentGroup extends Component {
   private components: Component[];
 
-  constructor(components: Component[]){
+  constructor(components: Component[] = []){
     super();
     this.components = components;
   }
 
-  public anchor(element: D3.Selection) {
+  public addComponent(c: Component): ComponentGroup {
+    this.components.push(c);
+    if (this.element != null) {
+      c.anchor(this.element.append("g"));
+    }
+    return this;
+  }
+
+  public anchor(element: D3.Selection): ComponentGroup {
     super.anchor(element);
     this.components.forEach((c) => c.anchor(this.element.append("g")));
     return this;
   }
 
-  public computeLayout(xOrigin?: number, yOrigin?: number, availableWidth?: number, availableHeight? : number){
+  public computeLayout(xOrigin?: number,
+                       yOrigin?: number,
+                availableWidth?: number,
+               availableHeight?: number): ComponentGroup {
     super.computeLayout(xOrigin, yOrigin, availableWidth, availableHeight);
     this.components.forEach((c) => {
       c.computeLayout(this.xOrigin, this.yOrigin, this.availableWidth, this.availableHeight);
