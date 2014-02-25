58317a1b387131af3cd78e409624f7500109fc2a
Justin Lan <jlan@palantir.com>
2014-02-24 17:15:18 -0800

    Add new indentation style to all demos.
    jsFiddle uses 4-space indents, not 2-space.

--- examples/chartWithLabels/demo.js
+++ examples/chartWithLabels/demo.js
@@ -1,29 +1,29 @@
 window.onload = function() {
-  var xMean = 0;
-  var xStdDev = 1;
-  var yMean = 0;
-  var yStdDev = 1;
-  var dataseries = {
-    seriesName: "normal-data",
-    data: makeNormallyDistributedData(100, xMean, xStdDev, yMean, yStdDev)
-  };
+    var xMean = 0;
+    var xStdDev = 1;
+    var yMean = 0;
+    var yStdDev = 1;
+    var dataseries = {
+        seriesName: "normal-data",
+        data: makeNormallyDistributedData(100, xMean, xStdDev, yMean, yStdDev)
+    };
 
-  var svg = d3.select("#chart-with-labels");
-  svg.attr("width", 480).attr("height", 320);
+    var svg = d3.select("#chart-with-labels");
+    svg.attr("width", 480).attr("height", 320);
 
-  var xScale = new LinearScale();
-  var xAxis = new XAxis(xScale, "bottom");
-  var xLabel = new AxisLabel("x"); // defaults to horizontal
-  var xAxisTable = new Table([[xAxis],
-                              [xLabel]]); // groups the x Axis and Label
+    var xScale = new LinearScale();
+    var xAxis = new XAxis(xScale, "bottom");
+    var xLabel = new AxisLabel("x"); // defaults to horizontal
+    var xAxisTable = new Table([[xAxis],
+                                [xLabel]]); // groups the x Axis and Label
 
-  var yScale = new LinearScale();
-  var yAxis = new YAxis(yScale, "left");
-  var yLabel = new AxisLabel("y", "vertical-left");
-  var yAxisTable = new Table([[yLabel, yAxis]]); // groups the y Axis and label
+    var yScale = new LinearScale();
+    var yAxis = new YAxis(yScale, "left");
+    var yLabel = new AxisLabel("y", "vertical-left");
+    var yAxisTable = new Table([[yLabel, yAxis]]); // groups the y Axis and label
 
-  var renderAreaD1 = new CircleRenderer(dataseries, xScale, yScale);
-  var basicTable = new Table([[yAxisTable, renderAreaD1],
-                              [null, xAxisTable]]);
-  basicTable.anchor(svg).computeLayout().render();
+    var renderAreaD1 = new CircleRenderer(dataseries, xScale, yScale);
+    var basicTable = new Table([[yAxisTable, renderAreaD1],
+                                [null, xAxisTable]]);
+    basicTable.anchor(svg).computeLayout().render();
 };
--- examples/selectionDemo/demo.js
+++ examples/selectionDemo/demo.js
@@ -1,28 +1,28 @@
 window.onload = function() {
-  var dataseries = makeRandomData(50);
+    var dataseries = makeRandomData(50);
 
-  var svg = d3.select("#selection-demo");
-  svg.attr("width", 480).attr("height", 320);
-  var xScale = new LinearScale();
-  var xAxis = new XAxis(xScale, "bottom");
-  var yScale = new LinearScale();
-  var yAxis = new YAxis(yScale, "left");
-  var renderAreaD1 = new CircleRenderer(dataseries, xScale, yScale);
-  var basicTable = new Table([[yAxis, renderAreaD1],
-                              [null, xAxis]]);
+    var svg = d3.select("#selection-demo");
+    svg.attr("width", 480).attr("height", 320);
+    var xScale = new LinearScale();
+    var xAxis = new XAxis(xScale, "bottom");
+    var yScale = new LinearScale();
+    var yAxis = new YAxis(yScale, "left");
+    var renderAreaD1 = new CircleRenderer(dataseries, xScale, yScale);
+    var basicTable = new Table([[yAxis, renderAreaD1],
+                                [null, xAxis]]);
 
-  var areaInteraction = new AreaInteraction(renderAreaD1); // attach a new AreaInteraction to the render area
-  var lastSelection = null; // initially, nothing is selected
-  var selectPoints = function(pixelArea) {
-    if (lastSelection != null) {
-      lastSelection.classed("seleted-point", false); // clear out the last set of selected points
-    }
-    var selectionArea = renderAreaD1.invertXYSelectionArea(pixelArea);
-    var selected = renderAreaD1.getSelectionFromArea(selectionArea);
-    selected.classed("selected-point", true); // set a selected class on the points
-    lastSelection = selected;
-  };
-  areaInteraction.callback(selectPoints); // add selectPoints as a callback when a selection is made
+    var areaInteraction = new AreaInteraction(renderAreaD1); // attach a new AreaInteraction to the render area
+    var lastSelection = null; // initially, nothing is selected
+    var selectPoints = function(pixelArea) {
+        if (lastSelection != null) {
+            lastSelection.classed("seleted-point", false); // clear out the last set of selected points
+        }
+        var selectionArea = renderAreaD1.invertXYSelectionArea(pixelArea);
+        var selected = renderAreaD1.getSelectionFromArea(selectionArea);
+        selected.classed("selected-point", true); // set a selected class on the points
+        lastSelection = selected;
+    };
+    areaInteraction.callback(selectPoints); // add selectPoints as a callback when a selection is made
 
-  basicTable.anchor(svg).computeLayout().render();
+    basicTable.anchor(svg).computeLayout().render();
 };
