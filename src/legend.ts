e732c58bdfa0c7d3453be0c303123b876db55b66
Daniel Mane <danmane@gmail.com>
2014-02-26 16:43:28 -0800

    Some code cleanup

--- src/legend.ts
+++ src/legend.ts
@@ -2,19 +2,17 @@
 
 class Legend extends Component {
   private static CSS_CLASS = "legend";
+  private static MARGIN = 5;
 
   private colorScale: ColorScale;
   private maxWidth: number;
-
-  private static RECT_MARGIN = 5;
-  private static TEXT_X_DISPLACEMENT = 5;
 
   constructor(colorScale?: ColorScale) {
     super();
     this.colMinimum(120); // the default width
     this.colorScale = colorScale;
     this.xAlign("RIGHT").yAlign("TOP");
-    // this.xOffset(5).yOffset(5);
+    this.xOffset(5).yOffset(5);
   }
 
   public scale(scale: ColorScale): Legend {
@@ -28,37 +26,40 @@ class Legend extends Component {
     if (newVal != null) {
       throw new Error("Row minimum cannot be directly set on Legend");
     } else {
-      var fakeLegendEl = this.element.append("g").classed(Legend.CSS_CLASS, true);
-      var textHeight = Utils.getTextHeight(fakeLegendEl.append("text"));
-      fakeLegendEl.remove();
+      var textHeight = this.measureTextHeight();
       return this.colorScale.domain().length * textHeight;
     }
   }
 
-  public render() {
-    super.render();
-    var domain = this.colorScale.domain();
-
+  private measureTextHeight(): number {
+    if (this.element == null) {
+      throw new Error("It is not presently possible to measure the text height before anchoring");
+    }
     var fakeLegendEl = this.element.append("g").classed(Legend.CSS_CLASS, true);
     var textHeight = Utils.getTextHeight(fakeLegendEl.append("text"));
     fakeLegendEl.remove();
+    return textHeight;
+  }
 
-    var availableWidth = this.colMinimum() - textHeight - Legend.RECT_MARGIN;
-
+  public render(): Legend {
+    super.render();
+    var domain = this.colorScale.domain();
+    var textHeight = this.measureTextHeight();
+    var availableWidth = this.colMinimum() - textHeight - Legend.MARGIN;
 
     var legend: D3.EnterSelection = this.element.selectAll(Legend.CSS_CLASS).data(domain)
       .enter()
         .append("g").classed(Legend.CSS_CLASS, true)
         .attr("transform", (d, i) => "translate(0," + i * textHeight + ")");
     legend.append("rect")
-        .attr("x", Legend.RECT_MARGIN)
-        .attr("y", Legend.RECT_MARGIN)
-        .attr("width",  textHeight - Legend.RECT_MARGIN * 2)
-        .attr("height", textHeight - Legend.RECT_MARGIN * 2)
+        .attr("x", Legend.MARGIN)
+        .attr("y", Legend.MARGIN)
+        .attr("width",  textHeight - Legend.MARGIN * 2)
+        .attr("height", textHeight - Legend.MARGIN * 2)
         .attr("fill", this.colorScale.scale);
     legend.append("text")
         .attr("x", textHeight)
-        .attr("y", Legend.RECT_MARGIN + textHeight / 2)
+        .attr("y", Legend.MARGIN + textHeight / 2)
         // .attr("dy", ".35em")
         .text(function(d, i) {return Utils.truncateTextToLength(d, availableWidth, d3.select(this));});
     return this;
