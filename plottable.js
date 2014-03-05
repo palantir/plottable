6d6c9fe117481df5b8e54bcb5bdbab0c487c9427
Ashwin Ramaswamy <aramaswamy@palantir.com>
2014-03-05 17:13:41 -0500

    v0.2.1

--- plottable.js
+++ plottable.js
@@ -1,5 +1,5 @@
 /*!
-Plottable v0.2.0 (https://github.com/palantir/plottable)
+Plottable v0.2.1 (https://github.com/palantir/plottable)
 Copyright 2014 Palantir Technologies
 Licensed under MIT (https://github.com/palantir/plottable/blob/master/LICENSE)
 */
