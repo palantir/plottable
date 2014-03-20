76ab1fc620f20d17d3f571f5d514dd3d3ef84ea0
Daniel Mane <danmane@gmail.com>
2014-03-19 17:56:26 -0700

    Work on building the homepage

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+window.onload = function() {
+  chart1();
+}
+
+
+
+function chart1() {
+  var svg = d3.select("#svg1");
+  var xScale = new Plottable.LinearScale();
+  var yScale = new Plottable.LinearScale();
+  var data = makeRandomData(20);
+  var renderer = new Plottable.CircleRenderer(data, xScale, yScale);
+  renderer.renderTo(svg);
+  xScale.padDomain();
+  yScale.padDomain();
+}
