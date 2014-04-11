f8005615f99101a708c95752ab094018243f1699
Daniel Mane <danmane@gmail.com>
2014-04-11 16:50:45 -0700

    Clean up return type on ordinalScale.domain

--- src/scales/ordinalScale.ts
+++ src/scales/ordinalScale.ts
@@ -16,13 +16,12 @@ module Plottable {
     }
 
     public domain(): any[];
-    public domain(values: any[]): Scale;
+    public domain(values: any[]): OrdinalScale;
     public domain(values?: any[]): any {
       if (values == null) {
         return this._d3Scale.domain();
       } else {
-        this._d3Scale.domain(values);
-        this._broadcasterCallbacks.forEach((b) => b(this));
+        super.domain(values);
         this._d3Scale.rangePoints(this.range(), 2*this.END_PADDING); // d3 scale takes total padding
         return this;
       }
