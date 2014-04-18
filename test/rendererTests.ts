b4cc08612820a2420f1c306f0d47dc91a95361b6
Bill Dwyer <bdwyer@palantir.com>
2014-04-17 17:02:12 -0700

    grids actually being drawn upside down.

--- test/rendererTests.ts
+++ test/rendererTests.ts
@@ -400,25 +400,25 @@ describe("Renderers", () => {
         assert.equal(cellAU.attr("height"), "100", "cell 'AU' height is correct");
         assert.equal(cellAU.attr("width"), "200", "cell 'AU' width is correct");
         assert.equal(cellAU.attr("x"), "0", "cell 'AU' x coord is correct");
-        assert.equal(cellAU.attr("y"), "0", "cell 'AU' x coord is correct");
+        assert.equal(cellAU.attr("y"), "100", "cell 'AU' x coord is correct");
         assert.equal(cellAU.attr("fill"), "#000000", "cell 'AU' color is correct");
 
         assert.equal(cellBU.attr("height"), "100", "cell 'BU' height is correct");
         assert.equal(cellBU.attr("width"), "200", "cell 'BU' width is correct");
         assert.equal(cellBU.attr("x"), "200", "cell 'BU' x coord is correct");
-        assert.equal(cellBU.attr("y"), "0", "cell 'BU' x coord is correct");
+        assert.equal(cellBU.attr("y"), "100", "cell 'BU' x coord is correct");
         assert.equal(cellBU.attr("fill"), "#212121", "cell 'BU' color is correct");
 
         assert.equal(cellAV.attr("height"), "100", "cell 'AV' height is correct");
         assert.equal(cellAV.attr("width"), "200", "cell 'AV' width is correct");
         assert.equal(cellAV.attr("x"), "0", "cell 'AV' x coord is correct");
-        assert.equal(cellAV.attr("y"), "100", "cell 'AV' x coord is correct");
+        assert.equal(cellAV.attr("y"), "0", "cell 'AV' x coord is correct");
         assert.equal(cellAV.attr("fill"), "#ffffff", "cell 'AV' color is correct");
 
         assert.equal(cellBV.attr("height"), "100", "cell 'BV' height is correct");
         assert.equal(cellBV.attr("width"), "200", "cell 'BV' width is correct");
         assert.equal(cellBV.attr("x"), "200", "cell 'BV' x coord is correct");
-        assert.equal(cellBV.attr("y"), "100", "cell 'BV' x coord is correct");
+        assert.equal(cellBV.attr("y"), "0", "cell 'BV' x coord is correct");
         assert.equal(cellBV.attr("fill"), "#777777", "cell 'BV' color is correct");
       };
 
@@ -433,6 +433,7 @@ describe("Renderers", () => {
         svg.remove();
       });
 
+
       it("renders correctly when data is set after construction", () => {
         var xScale: Plottable.OrdinalScale = new Plottable.OrdinalScale();
         var yScale: Plottable.OrdinalScale = new Plottable.OrdinalScale();
@@ -442,6 +443,43 @@ describe("Renderers", () => {
         renderer.renderTo(svg);
         renderer.dataSource().data(DATA);
         VERIFY_CELLS(renderer.renderArea.selectAll("rect")[0]);
+        svg.remove();
+      });
+
+      it("can invert y axis correctly", () => {
+        var xScale: Plottable.OrdinalScale = new Plottable.OrdinalScale();
+        var yScale: Plottable.OrdinalScale = new Plottable.OrdinalScale();
+        var colorScale: Plottable.InterpolatedColorScale = new Plottable.InterpolatedColorScale(["black", "white"]);
+        var svg: D3.Selection = generateSVG(SVG_WIDTH, SVG_HEIGHT);
+        var renderer: Plottable.GridRenderer = new Plottable.GridRenderer(null, xScale, yScale, colorScale, "x", "y", "magnitude");
+        renderer.renderTo(svg);
+
+        yScale.domain(["U", "V"]);
+        renderer.dataSource().data(DATA);
+
+        var cells = renderer.renderArea.selectAll("rect")[0];
+        var cellAU = d3.select(cells[0]);
+        var cellAV = d3.select(cells[2]);
+        cellAU.attr("fill", "#000000");
+        cellAU.attr("x", "0");
+        cellAU.attr("y", "100");
+
+        cellAV.attr("fill", "#ffffff");
+        cellAV.attr("x", "0");
+        cellAV.attr("y", "0");
+
+        yScale.domain(["V", "U"]);
+        cells = renderer.renderArea.selectAll("rect")[0];
+        cellAU = d3.select(cells[0]);
+        cellAV = d3.select(cells[2]);
+        cellAU.attr("fill", "#000000");
+        cellAU.attr("x", "0");
+        cellAU.attr("y", "0");
+
+        cellAV.attr("fill", "#ffffff");
+        cellAV.attr("x", "0");
+        cellAV.attr("y", "100");
+
         svg.remove();
       });
     });
