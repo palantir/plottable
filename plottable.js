4f5052ab8e4e81a7d8528dce6e34517652681c46
Daniel Mane <danmane@gmail.com>
2014-04-28 18:49:53 -0700

    Update plottable.js, plottable.d.ts, and other build artefacts

--- plottable.js
+++ plottable.js
@@ -1970,6 +1970,14 @@ var Plottable;
             this.renderArea = this.content.append("g").classed("render-area", true);
             return this;
         };
+
+        Renderer.prototype.animate = function (toggle) {
+            if (toggle == null) {
+                toggle = !this._animate;
+            }
+            this._animate = toggle;
+            return this;
+        };
         Renderer.DEFAULT_COLOR_ACCESSOR = function (d) {
             return "#1f77b4";
         };
@@ -2052,7 +2060,7 @@ var Plottable;
             this.classed("circle-renderer", true);
             this.project("r", 3); // default
             this.project("fill", function () {
-                return "#00ffaa";
+                return "steelblue";
             }); // default
         }
         CircleRenderer.prototype.project = function (attrToSet, accessor, scale) {
