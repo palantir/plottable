1d03959e9fdb26b0764cac567cf85b34d1b23db2
Daniel Mane <danmane@gmail.com>
2014-05-01 12:30:27 -0700

    Make the customProjector tutorial show off more stuff

--- tutorials/customProjectors/customProjectors.js
+++ tutorials/customProjectors/customProjectors.js
@@ -6,15 +6,35 @@ window.onload = function() {
   var yAxis = new Plottable.YAxis(yScale, "left");
   var renderer = new Plottable.LineRenderer(gitData, xScale, yScale);
 
-  function getXDataValue(d) {
+  // A DataSource is a Plottable object that maintains data and metadata, and updates dependents when it changes
+  // In the previous example, we implicitly created a DataSource by putting the data directly into the Renderer constructor
+  var gitDataSource = new Plottable.DataSource(gitData);
+  var renderer = new Plottable.CircleRenderer(gitDataSource, xScale, yScale);
+
+  // We define an accessor function that the renderer will use to access a "Perspective" into the DataSource
+  function dayAccessor(d) {
     return d.day;
   }
   renderer.project("x", getXDataValue, xScale);
 
-  function getYDataValue(d) {
-    return d.total_commits;
+  // Make a LogScale. Since the range doesn't correspond to the layout of a renderer, we set the range manually.
+  var radiusScale = new Plottable.LinearScale().range([1, 10]);
+
+  function linesChangedAccessor(d) {
+    return Math.sqrt(Math.max(d.additions - d.deletions, 1));
   }
-  renderer.project("y", getYDataValue, yScale);
+
+  renderer.project("r", linesChangedAccessor, radiusScale)
+
+  function ratioAdditionsToDeletions(d) {
+    if (d.deletions + d.additions === 0) {
+      return 0;
+    }
+    return d.deletions / (d.deletions + d.additions);
+  }
+  // The InterpolatedColorScale will interpolate between blue for low values and red for high values
+  var colorScale = new Plottable.InterpolatedColorScale("posneg");
+  renderer.project("fill", ratioAdditionsToDeletions, colorScale);
 
   var chart = new Plottable.Table([
                     [yAxis, renderer],
