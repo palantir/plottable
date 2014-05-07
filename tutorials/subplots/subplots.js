68e6d42089dea1b56a57420bcfab6fb12c3c8942
Justin Lan <jlan@palantir.com>
2014-05-07 15:26:49 -0700

    Add subplots tutorial.

---
+++ tutorials/subplots/subplots.js
@@ -0,0 +1,36 @@
+function makeChartWithSubplots() {
+  var xScale = new Plottable.LinearScale();
+  var xAxis = new Plottable.XAxis(xScale, "bottom");
+
+  var lineYScale = new Plottable.LinearScale();
+  var lineYAxis = new Plottable.YAxis(lineYScale, "left");
+  var lineRenderer = new Plottable.LineRenderer(gitData, xScale, lineYScale);
+
+  var circleYScale = new Plottable.LinearScale();
+  var circleYAxis = new Plottable.YAxis(circleYScale, "left");
+  var circleRenderer = new Plottable.CircleRenderer(gitData, xScale, circleYScale);
+
+  function getDayValue(d) {
+    return d.day;
+  }
+  lineRenderer.project("x", getDayValue, xScale);
+  circleRenderer.project("x", getDayValue, xScale);
+
+  function getTotalCommits(d) {
+    return d.total_commits;
+  }
+  lineRenderer.project("y", getTotalCommits, lineYScale);
+
+  function getNetCommitSize(d) {
+    return d.additions - d.deletions;
+  }
+  circleRenderer.project("y", getNetCommitSize, circleYScale);
+
+  var chart = new Plottable.Table([
+                    [lineYAxis,   lineRenderer],
+                    [circleYAxis, circleRenderer],
+                    [null,        xAxis   ]
+                  ]);
+
+  chart.renderTo("#customProjectorChart");
+}
