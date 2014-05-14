2dbd1f611e2552bce086c77a2f19811a8e90f704
Daniel Mane <danmane@gmail.com>
2014-05-13 21:24:04 -0700

    Fix compiler error

--- test/tableTests.ts
+++ test/tableTests.ts
@@ -221,7 +221,7 @@ describe("Tables", () => {
     makeComponentFixedSize(c2, 20, 50);
     makeComponentFixedSize(c3, 20, 20);
 
-    function verifySpaceRequest(sr: Plottable.SpaceRequest, w: number, h: number, ww: boolean, wh: boolean, id: string) {
+    function verifySpaceRequest(sr: Plottable.ISpaceRequest, w: number, h: number, ww: boolean, wh: boolean, id: string) {
       assert.equal(sr.width, w, "width requested is as expected #" + id);
       assert.equal(sr.height, h, "height requested is as expected #" + id);
       assert.equal(sr.wantsWidth, ww, "needs more width is as expected #" + id);
