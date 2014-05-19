7c65402d2ed45d972b5d9ba9193efc514cf3abfb
Justin Lan <jlan@palantir.com>
2014-05-19 14:52:21 -0700

    Add Bar Chart tutorial.

---
+++ tutorials/barChart/barChart.js
@@ -0,0 +1,19 @@
+function makeBarChart() {
+  var xScale = new Plottable.OrdinalScale().rangeType("bands");
+  var yScale = new Plottable.LinearScale();
+
+  var xAxis = new Plottable.XAxis(xScale, "bottom", function(d) { return d; });
+  var yAxis = new Plottable.YAxis(yScale, "left");
+  var renderer = new Plottable.BarRenderer(barData, xScale, yScale)
+                              .project("x", "category", xScale)
+                              .project("y", "value", yScale)
+                              .project("fill", function() { return "steelblue"; } );
+  yScale.padDomain();
+
+  var chart = new Plottable.Table([
+                    [yAxis, renderer],
+                    [null,  xAxis   ]
+                  ]);
+
+  chart.renderTo("#bar-chart");
+}
