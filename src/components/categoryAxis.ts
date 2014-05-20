c79ab39dcb94a30bef21a000045927feea129fa1
Daniel Mane <danmane@gmail.com>
2014-05-20 15:50:14 -0700

    Initial implementation of catAxis, needs TextUtils.drawTextToBox implementation

--- src/components/categoryAxis.ts
+++ src/components/categoryAxis.ts
@@ -4,6 +4,7 @@ module Plottable {
   export class CategoryAxis extends Component {
     private _scale: OrdinalScale;
     private orientation: string;
+    private _height = 50;
 
     constructor(scale: OrdinalScale, orientation = "bottom") {
       super();
@@ -12,7 +13,32 @@ module Plottable {
       if (orientation.toLowerCase() !== "bottom") {
         throw new Error("Only bottom-oriented axes are implemented");
       }
+      if (scale.rangeType() !== "bands") {
+        throw new Error("Only rangeBands category axes are implemented");
+      }
       this._registerToBroadcaster(this._scale, () => this._invalidateLayout());
+    }
+
+    public height(newHeight: number) {
+      this._height = newHeight;
+      return this;
+    }
+
+    public _requestedSpace(offeredWidth: number, offeredHeight: number) {
+      return {
+        width: 0,
+        height: Math.min(offeredHeight, this._height),
+        wantsWidth: false,
+        wantsHeight: offeredHeight < this._height
+      };
+    }
+
+    public _doRender() {
+      var bandWidth: number = this._scale.rangeBand();
+      this._scale.domain().forEach((s: string) => {
+        var bandStartPosition: number = this._scale.scale(s);
+        TextUtils.drawTextToBox(s, bandStartPosition, 0, bandWidth, this._height);
+      });
     }
 
     private layoutHorizontallyWithoutBreakingWords(width: number, height: number) {
