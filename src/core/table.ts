3021d77620c3f24c963bf2344477c284c01b8d8f
Daniel Mane <danmane@gmail.com>
2014-05-22 15:42:50 -0700

    Instead of unconditionally giving space to unsatisfied components, Table instead treats them as having a low proportional weight. Ensures that category axes cannot steal all of the space in a Table.

--- src/core/table.ts
+++ src/core/table.ts
@@ -124,14 +124,16 @@ module Plottable {
         freeHeight = availableHeightAfterPadding - d3.sum(guarantees.guaranteedHeights);
         var xWeights: number[];
         if (wantsWidth) { // If something wants width, divide free space between components that want more width
-          xWeights = guarantees.wantsWidthArr.map((x) => x ? 1 : 0);
+          xWeights = guarantees.wantsWidthArr.map((x) => x ? 0.2 : 0);
+          xWeights = Utils.addArrays(xWeights, colWeights);
         } else { // Otherwise, divide free space according to the weights
           xWeights = colWeights;
         }
 
         var yWeights: number[];
         if (wantsHeight) {
-          yWeights = guarantees.wantsHeightArr.map((x) => x ? 1 : 0);
+          yWeights = guarantees.wantsHeightArr.map((x) => x ? 0.2 : 0);
+          yWeights = Utils.addArrays(xWeights, rowWeights);
         } else {
           yWeights = rowWeights;
         }
