d140f84ca231a6a982b50ca4e330add9ee6e1525
lewin <lg52931@berkeley.edu>
2014-05-27 14:40:59 -0700

    Update built files

--- test/tests.js
+++ test/tests.js
@@ -3219,11 +3219,11 @@ describe("Tables", function () {
             assert.deepEqual(table.rows, [[null, c2, c3], [c4, c5, c6]], "item twice");
         });
 
-        it("table._removeComponent for multiple copies of the same item", function () {
-            table = new Plottable.Table([[c1, c1], [c2, c1]]);
+        it("table._removeComponent doesn't do anything weird when called with null", function () {
+            table = new Plottable.Table([[c1, null], [c2, c3]]);
 
-            table._removeComponent(c1);
-            assert.deepEqual(table.rows, [[c2]], "remove once");
+            table._removeComponent(null);
+            assert.deepEqual(table.rows, [[c1, null], [c2, c3]]);
         });
     });
 });
