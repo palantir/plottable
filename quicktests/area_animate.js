a3486302f1f135c5ddc9a90e031d22a0c0bd154b
crmorford <cmorford@palantir.com>
2014-06-05 17:51:23 -0700

    Create area_animate.js

---
+++ quicktests/area_animate.js
@@ -0,0 +1,21 @@
+var quicktests = (quicktests || []);
+
+var quicktest = function(svg, data, Plottable) {
+
+      var areaRenderer;
+      var xScale = new Plottable.Scale.Linear();
+      var xAxis = new Plottable.Axis.XAxis(xScale, "bottom");
+
+      var yScale = new Plottable.Scale.Linear();
+      var yAxis = new Plottable.Axis.YAxis(yScale, "left");
+
+      areaRenderer = new Plottable.Plot.Area(data[0].slice(0, 21), xScale, yScale);
+      areaRenderer.project("opacity", 0.75);
+      areaRenderer.animate(doAnimate);
+
+      var areaChart = new Plottable.Table([[yAxis, areaRenderer],
+                                               [null,  xAxis]]);
+      areaChart.renderTo(svg);
+}
+
+quicktests.push(quicktest);
