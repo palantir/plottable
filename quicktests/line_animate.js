a0b56b5fb0292d954554dfa5687a54cc7fd07d05
crmorford <cmorford@palantir.com>
2014-06-05 17:52:52 -0700

    Create line_animate.js

---
+++ quicktests/line_animate.js
@@ -0,0 +1,21 @@
+var quicktests = (quicktests || []);
+
+var quicktest = function(svg, data, Plottable) {
+
+      var lineRenderer;
+      var xScale = new Plottable.Scale.Linear();
+      var xAxis = new Plottable.Axis.XAxis(xScale, "bottom");
+
+      var yScale = new Plottable.Scale.Linear();
+      var yAxis = new Plottable.Axis.YAxis(yScale, "left");
+
+      lineRenderer = new Plottable.Plot.Line(data[0].slice(0, 21), xScale, yScale);
+      lineRenderer.project("opacity", 0.75);
+      lineRenderer.animate(doAnimate);
+
+      var lineaChart = new Plottable.Table([[yAxis, lineRenderer],
+                                               [null,  xAxis]]);
+      lineChart.renderTo(svg);
+}
+
+quicktests.push(quicktest);
