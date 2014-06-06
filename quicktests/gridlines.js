c3070525e2c30a5ec19491635c0885e02cb90448
crmorford <cmorford@palantir.com>
2014-06-06 10:59:55 -0700

    Update gridlines.js

--- quicktests/gridlines.js
+++ quicktests/gridlines.js
@@ -1,20 +1,21 @@
 var quicktest = function(svg, data, Plottable) {
 
-  xScale = new Plottable.Scale.Linear();
-  xAxis = new Plottable.Axis.XAxis(xScale, "bottom");
+  var xScale = new Plottable.Scale.Linear();
+  var xAxis = new Plottable.Axis.XAxis(xScale, "bottom");
 
-  yScale = new Plottable.Scale.Linear();
-  yAxis = new Plottable.Axis.YAxis(yScale, "left").tickLabelPosition("bottom");
-  yAxis.tickFormat(d3.format("g"));
+  varyScale = new Plottable.Scale.Linear();
+  var yAxis = new Plottable.Axis.YAxis(yScale, "left").tickLabelPosition("bottom");
+  //yAxis.tickFormat(d3.format("g"));
 
-  renderAreaD1 = new Plottable.Plot.Scatter(data, xScale, yScale);
-  gridlines = new Plottable.Component.Gridlines(xScale, yScale);
-  renderGroup = renderAreaD1.merge(gridlines);
-  chart = new Plottable.Component.Table([[yAxis, renderGroup],
+  var renderAreaD1 = new Plottable.Plot.Scatter(data, xScale, yScale);
+  var gridlines = new Plottable.Component.Gridlines(xScale, yScale);
+  var renderGroup = renderAreaD1.merge(gridlines);
+  var chart = new Plottable.Component.Table([[yAxis, renderGroup],
                                          [null,  xAxis]]);
-  chart.renderTo(svg);
   var pzi = new Plottable.Interaction.PanZoom(renderGroup, xScale, yScale);
   pzi.registerWithComponent();
+  chart.renderTo(svg);
+
 }
 
 quicktest.quicktestName = "gridlines";
