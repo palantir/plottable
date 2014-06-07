f1b1d0cd38e09bea8293ff192233fe82f0bdf039
Daniel Mane <danmane@gmail.com>
2014-06-06 17:02:42 -0700

    Remove the lineRenderer and second axis

--- examples/new-page-for-tak/issueschart.js
+++ examples/new-page-for-tak/issueschart.js
@@ -1,7 +1,6 @@
 function makeIssuesChart(svg, data) {
   var xScale = new Plottable.Scale.Time();
   var yScale = new Plottable.Scale.Linear();
-  var yScale2 = new Plottable.Scale.Linear();
   var format = d3.time.format("%Y/%m/%d");
   var closedLine = new Plottable.Plot.Area(data, xScale, yScale)
                         .project("x", "parsedDate", xScale)
@@ -15,27 +14,15 @@ function makeIssuesChart(svg, data) {
                         .project("y0", "nClosed", yScale)
                         .project("fill", function() {return "red"});
 
+  var gridlines = new Plottable.Component.Gridlines(xScale, yScale);
+  var group = closedLine.merge(openArea).merge(gridlines);
 
   var xAxis = new Plottable.Axis.XAxis(xScale, "bottom", format);
-  var yAxisLeft = new Plottable.Axis.YAxis(yScale, "left");
-  var yAxisRight = new Plottable.Axis.YAxis(yScale2, "right");
+  var yAxis = new Plottable.Axis.YAxis(yScale, "left");
+  var titleLabel = new Plottable.Component.TitleLabel("Issues Over Time")
 
-  var titleLabel = new Plottable.Component.TitleLabel("Issues Over Time");
-
-  var line = new Plottable.Plot.Line(data, xScale, yScale2)
-                        .project("x", "parsedDate", xScale)
-                        .project("y", "nOpen", yScale2);
-
-  var group = closedLine.merge(openArea).merge(line);
-  var table = new Plottable.Component.Table([
-                                  [null, titleLabel, null],
-                                  [yAxisLeft, group, yAxisRight],
-                                  [null, xAxis, null]])
-                            .renderTo(svg);
-
-
-  // var chart = new Plottable.Template.StandardChart().center(group)
-  //                                                   .xAxis(xAxis).yAxis(yAxis)
-  //                                                   .titleLabel("Plottable - Issues over Time")
-  //                                                   .renderTo(svg);
+  var chart = new Plottable.Template.StandardChart().center(group)
+                                                    .xAxis(xAxis).yAxis(yAxis)
+                                                    .titleLabel("Plottable - Issues over Time")
+                                                    .renderTo(svg);
 }
