21b5bfe23be9ad4c3f785adc20451ab2a7c81946
crmorford <cmorford@palantir.com>
2014-06-09 11:56:00 -0700

    Update list_of_quicktests.js

--- quicktests/list_of_quicktests.js
+++ quicktests/list_of_quicktests.js
@@ -1,10 +1,3 @@
 var list_of_quicktests = [
-  "area_animate",
-  "areaRenderer",
-  "dragzoom_x",
-  "panzoom",
-  "LayoutInvalidate",
-  "line_animate",
-  "scatter_animate",
-  "verticalBar_animate"
+  "basic_area"
 ]
