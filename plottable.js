9e8b03c09de7fd2dc1bdbd6caa1a439ea179474d
Daniel Mane <danmane@gmail.com>
2014-06-09 15:08:12 -0700

    Release version 0.15.3

--- plottable.js
+++ plottable.js
@@ -1,5 +1,5 @@
 /*!
-Plottable 0.15.2 (https://github.com/palantir/plottable)
+Plottable 0.15.3 (https://github.com/palantir/plottable)
 Copyright 2014 Palantir Technologies
 Licensed under MIT (https://github.com/palantir/plottable/blob/master/LICENSE)
 */
