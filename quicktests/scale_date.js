b99ea7052ab4ae3d503334d619277741fdfbfcf4
crmorford <cmorford@palantir.com>
2014-06-09 14:29:50 -0700

    Update scale_date.js

--- quicktests/scale_date.js
+++ quicktests/scale_date.js
@@ -1,4 +1,4 @@
-var data = [
+var dataPts = [
     {x: "5/2/2014", y: 2},
     {x: "5/3/2014", y: 3},
     {x: "5/6/2014", y: 4}
@@ -9,7 +9,7 @@ var quicktest = function(svg, data, Plottable) {
 
     xScale = new Plottable.Scale.Time();
     yScale = new Plottable.Scale.Linear();
-    linePlot = new Plottable.Plot.Line(data, xScale, yScale)
+    linePlot = new Plottable.Plot.Line(dataPts, xScale, yScale)
                                 .project("x", function (d) { return d3.time.format("%x").parse(d.x)});
     xAxis = new Plottable.Axis.XAxis(xScale);
     yAxis = new Plottable.Axis.YAxis(yScale);
