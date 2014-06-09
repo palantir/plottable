d2717de2eb94690fd92def242b01dbbcdf3f68ed
crmorford <cmorford@palantir.com>
2014-06-09 13:35:18 -0700

    Update list_of_quicktests.js

--- quicktests/list_of_quicktests.js
+++ quicktests/list_of_quicktests.js
@@ -1,11 +1,12 @@
 var list_of_quicktests = [
-  "animate_area",
-  "animate_horizontalBar",
-  "animate_line",
-  "animate_scatter",
-  "animate_verticalBar",
-  "interaction_dragzoomx",
-  "interaction_panzoom",
-  "legend_basic",
-  "basic_area"
+  {name:"animate_area", categories:["area", "animate"]},
+  {name:"animate_horizontalBar", categories:["horizontalbar", "animate"]},
+  {name:"animate_scatter", categories:["scatter", "animate"]},
+  {name:"animate_verticalBar", categories:["verticalbar", "animate"]},
+  {name:"interaction_dragzoomx", categories:["area", "interaction"]},
+  {name:"interaction_panzoom", categories:["scatter", "gridlines", "interaction"]},
+  {name:"legend_basic", categories:["layout", "legend", "scatter", "line", "title"]},
+  {name:"basic_area", categories:["area", "basic"]}
 ]
+
+
