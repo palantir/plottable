d8f132a17770973d53423f40d9c62fdcca45f03a
Bill Dwyer <bdwyer@palantir.com>
2014-06-11 11:09:14 -0700

    split resize broadcaster into separate file. add accessor to resize flag. null check animator.

--- src/core/renderController.ts
+++ src/core/renderController.ts
@@ -2,6 +2,7 @@
 
 module Plottable {
 export module Singleton {
+
   /**
    * The RenderController is responsible for enqueueing and synchronizing
    * layout and render calls for Plottable components.
@@ -91,58 +92,5 @@ export module Singleton {
     }
   }
 
-  /**
-   * The ResizeBroadcaster will broadcast a notification to any registered
-   * components when the window is resized.
-   *
-   * The broadcaster and single event listener are lazily constructed.
-   *
-   * Upon resize, the _resized flag will be set to true until after the next
-   * flush of the RenderController. This is used, for example, to disable
-   * animations during resize.
-   */
-  export class ResizeBroadcaster {
-    private static _broadcaster: Abstract.Broadcaster;
-    public static _resized: boolean = false;
-
-    private static _lazyInitialize() {
-      if (ResizeBroadcaster._broadcaster === undefined) {
-        ResizeBroadcaster._broadcaster = new Abstract.Broadcaster();
-        window.addEventListener("resize", ResizeBroadcaster._onResize);
-      }
-    }
-
-    private static _onResize(){
-      ResizeBroadcaster._resized = true;
-      ResizeBroadcaster._broadcaster._broadcast();
-    }
-
-    /**
-     * Registers a component.
-     *
-     * When the window is resized, we invoke ._invalidateLayout() on the
-     * component, which will enqueue the component for layout and rendering
-     * with the RenderController.
-     *
-     * @param {Abstract.Component} component Any Plottable component.
-     */
-    public static register(c: Abstract.Component) {
-      ResizeBroadcaster._lazyInitialize();
-      ResizeBroadcaster._broadcaster.registerListener(c._plottableID, () => c._invalidateLayout());
-    }
-
-    /**
-     * Deregisters the components.
-     *
-     * The component will no longer receive updates on window resize.
-     *
-     * @param {Abstract.Component} component Any Plottable component.
-     */
-    public static deregister(c: Abstract.Component) {
-      if (ResizeBroadcaster._broadcaster) {
-        ResizeBroadcaster._broadcaster.deregisterListener(c._plottableID);
-      }
-    }
-  }
 }
 }
