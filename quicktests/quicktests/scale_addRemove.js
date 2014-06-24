5f39ab7eb6a0694a25a7d17ec08f24afa06e4527
first_name last_name <email address>
2014-06-23 18:01:32 -0700

    scale_addRemove goes click-by-click

--- quicktests/quicktests/scale_addRemove.js
+++ quicktests/quicktests/scale_addRemove.js
@@ -3,20 +3,23 @@ var xScale1, xScale2, yScale1, yScale2;
 
 var renderArea1, renderArea2, renderArea3, renderArea4;
 
-    //data
-    var dataseries = new Plottable.DataSource(data[0].slice(0, 10));
-    dataseries.metadata({name: "series1"});
-    var dataseries2 = new Plottable.DataSource(data[1].slice(0, 10));
-    dataseries2.metadata({name: "series2"});
-    var dataseries3 = new Plottable.DataSource(data[0].slice(10, 20));
-    dataseries3.metadata({name: "series3"});
-    var dataseries4 = new Plottable.DataSource(data[1].slice(10, 20));
-    dataseries4.metadata({name: "series4"});
+var d_array = [];
+var scale_array = [];
+var axis_array = [];
+
+var clicks = 0;
+
+    for(var i = 0; i < 4; i++){
+        var data_sliced = (i>2)?data[i%2].slice(0, 10):data[i%2].slice(10, 20);
+        d_array[i] = new Plottable.DataSource(data_sliced);
+        d_array[i].metadata({name: "series" + (i+1).toString()});
+        console.log(d_array[i].metadata);
+    }
+
+
     var colorScale1 = new Plottable.Scale.Color("20");
     colorScale1.domain(["series1", "series2", "series3", "series4"]);
     
-    //Axis
-
     xScale1 = new Plottable.Scale.Linear();
     yScale1 = new Plottable.Scale.Linear(); 
     xScale2 = new Plottable.Scale.Linear();
@@ -30,92 +33,49 @@ var renderArea1, renderArea2, renderArea3, renderArea4;
        return colorScale1.scale(m.name);
     };
     
-    //rendering
-    renderArea1 = new Plottable.Plot.Area(dataseries, xScale1, yScale1);
-    renderArea1.project("fill", colorProjector);
-    renderArea2 = new Plottable.Plot.Area(dataseries2, xScale1, yScale1);
-    renderArea2.project("fill", colorProjector);    
-    renderArea3 = new Plottable.Plot.Area(dataseries3, xScale2, yScale2);
-    renderArea3.project("fill", colorProjector);
-    renderArea4 = new Plottable.Plot.Area(dataseries4, xScale2, yScale2);
-    renderArea4.project("fill", colorProjector);
+
+    var plot_array = [];
+    for(var j = 0; j < 4; j++){
+        plot_array[j] = new Plottable.Plot.Area(d_array[j], (j<2)?xScale1:xScale2, (j<2)?yScale1:yScale2);
+        plot_array[j].project("fill", colorProjector);
+    }
     
     
-    renderGroup = renderArea1.merge(renderArea2);
-    renderGroup2 = renderArea3.merge(renderArea4);  
+    renderGroup = plot_array[0].merge(plot_array[1]);
+    renderGroup2 = plot_array[2].merge(plot_array[3]);  
     
     var basicTable = new Plottable.Component.Table()
                 .addComponent(0, 0, yAxis)
                 .addComponent(0, 1, renderGroup)
-                .addComponent(1, 1, xAxis);
-    var basicTable2 = new Plottable.Component.Table()
-                .addComponent(0, 0, yAxis2)
-                .addComponent(0, 1, renderGroup2)
-                .addComponent(1, 1, xAxis2);
-    
-    var bigTable = new Plottable.Component.Table()
-                .addComponent(0, 0, basicTable)
-                .addComponent(1, 0, basicTable2);
+                .addComponent(1, 1, xAxis)
+                .addComponent(2, 0, yAxis2)
+                .addComponent(2, 1, renderGroup2)
+                .addComponent(3, 1, xAxis2);
+    basicTable.renderTo(svg);
 
-    bigTable.renderTo(svg);
 
 addRemove = function(x, y){
-    var gap = 1000;
-    setTimeout(function(){Area1Group1()}, gap*1);
-    setTimeout(function(){Area2Group1()}, gap*2);
-    setTimeout(function(){Area3Group1()}, gap*3);
-    setTimeout(function(){area4Group1()}, gap*4);
-    setTimeout(function(){Area1Group2()}, gap*5);
-    setTimeout(function(){Area2Group2()}, gap*6);
-    setTimeout(function(){Area3Group2()}, gap*7);
-    setTimeout(function(){Area4Group2()}, gap*8);
+
+    var rg = (clicks<4)?renderGroup:renderGroup2;
+    var x = (clicks<4)?xScale1:xScale2;
+    var y = (clicks<4)?yScale1:yScale2;
+    var plot = plot_array[clicks%4];
+    movePlot(rg, plot, x, y);
+    clicks++;
+    if(clicks == 8){ clicks = 0;}
 }
 
 
-window.xy = new Plottable.Interaction.Click(bigTable)
+window.xy = new Plottable.Interaction.Click(basicTable)
     .callback(addRemove)
     .registerWithComponent();
 
-function Area1Group1() {
-    project_newScale(renderArea1, xScale1, yScale1);
-    renderGroup.merge(renderArea1);    
-}
-function Area2Group1() {
-    project_newScale(renderArea2, xScale1, yScale1);
-    renderGroup.merge(renderArea2);    
-}
-function Area3Group1() {
-    project_newScale(renderArea3, xScale1, yScale1);
-    renderGroup.merge(renderArea3);    
-}
-function area4Group1() {
-    project_newScale(renderArea4, xScale1, yScale1);
-    renderGroup.merge(renderArea4);    
-}
-function Area1Group2() {
-    project_newScale(renderArea1, xScale2, yScale2);
-    renderGroup2.merge(renderArea1);    
-}
-function Area2Group2() {
-    project_newScale(renderArea2, xScale2, yScale2);
-    renderGroup2.merge(renderArea2);    
-}
-function Area3Group2() {
-    project_newScale(renderArea3, xScale2, yScale2);
-    renderGroup2.merge(renderArea3);    
-}
-function Area4Group2() {
-    project_newScale(renderArea4, xScale2, yScale2);
-    renderGroup2.merge(renderArea4);    
-}
-function xAccessor(d){
-    return d.x;
-}
-function yAccessor(d){
-    return d.y;
+function movePlot(renderGroup, renderArea, xScale, yScale){
+    project_newScale(renderArea, xScale, yScale);
+    renderGroup.merge(renderArea);
 }
 function project_newScale(renderArea, xScale, yScale){
     renderArea.remove();
-    renderArea.project("x", xAccessor, xScale);
-    renderArea.project("y", yAccessor, yScale);
+    renderArea.project("x", "x", xScale);
+    renderArea.project("y", "y", yScale);
 }
