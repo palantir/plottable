eaa3f2a153e545f1df5a921f825386ca7e85141d
Brian Malehorn <bmalehorn@gmail.com>
2014-07-08 14:09:06 -0700

    test for scale.range() changes

--- test/core/domainerTests.ts
+++ test/core/domainerTests.ts
@@ -36,8 +36,18 @@ describe("Domainer", () => {
   it("pad() works on log scales", () => {
     var logScale = new Plottable.Scale.Log();
     logScale.updateExtent(1, "x", [10, 100]);
+    logScale.range([0, 1]);
     logScale.domainer(domainer.pad(2.0));
-    assert.deepEqual(logScale.domain(), [1, 1000]);
+    assert.closeTo(logScale.domain()[0], 1, 0.001);
+    assert.closeTo(logScale.domain()[1], 1000, 0.001);
+    logScale.range([50, 60]);
+    logScale.autoDomain();
+    assert.closeTo(logScale.domain()[0], 1, 0.001);
+    assert.closeTo(logScale.domain()[1], 1000, 0.001);
+    logScale.range([-1, -2]);
+    logScale.autoDomain();
+    assert.closeTo(logScale.domain()[0], 1, 0.001);
+    assert.closeTo(logScale.domain()[1], 1000, 0.001);
   });
 
   it("pad() defaults to [v-1, v+1] if there's only one numeric value", () => {
