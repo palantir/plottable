4b3d5376a1a05b34ab60d516f8a4e0e038917338
lewin <lg52931@berkeley.edu>
2014-07-14 10:22:48 -0700

    added build number

--- Gruntfile.js
+++ Gruntfile.js
@@ -287,6 +287,7 @@ module.exports = function(grunt) {
           urls: ['http://127.0.0.1:9999/test/tests.html'],
           testname: 'Plottable Sauce Unit Tests',
           browsers: browsers,
+          build: process.env.TRAVIS_JOB_ID,
         }
       }
     }
