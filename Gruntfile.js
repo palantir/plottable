353003b8a594cb6385fda5533c1562a4d7e6318f
lewin <lg52931@berkeley.edu>
2014-07-14 10:41:36 -0700

    added tunnel-identifier

--- Gruntfile.js
+++ Gruntfile.js
@@ -288,6 +288,7 @@ module.exports = function(grunt) {
           testname: 'Plottable Sauce Unit Tests',
           browsers: browsers,
           build: process.env.TRAVIS_JOB_ID,
+          "tunnel-identifier": process.env.TRAVIS_JOB_NUMBER
         }
       }
     }
