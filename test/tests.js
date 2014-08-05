5805d57f0562d31a527980adda08f629255caeb8
lewin <lg52931@berkeley.edu>
2014-08-05 10:47:31 -0700

    added after check for any svgs

--- test/tests.js
+++ test/tests.js
@@ -2,7 +2,7 @@ function generateSVG(width, height) {
     if (width === void 0) { width = 400; }
     if (height === void 0) { height = 400; }
     var parent = getSVGParent();
-    return parent.append("svg").attr("width", width).attr("height", height);
+    return parent.append("svg").attr("width", width).attr("height", height).attr("class", "svg");
 }
 function getSVGParent() {
     var mocha = d3.select("#mocha-report");
@@ -93,6 +93,20 @@ var MultiTestVerifier = (function () {
 before(function () {
     Plottable.Core.RenderController.setRenderPolicy(new Plottable.Core.RenderController.RenderPolicy.Immediate());
     window.Pixel_CloseTo_Requirement = window.PHANTOMJS ? 2 : 0.5;
+});
+after(function () {
+    var parent = getSVGParent();
+    var mocha = d3.select("#mocha-report");
+    if (mocha.node() != null) {
+        var suites = mocha.selectAll(".suite");
+        for (var i = 0; i < suites[0].length; i++) {
+            var curSuite = d3.select(suites[0][i]);
+            assert(curSuite.selectAll("ul").selectAll("svg").node() === null, "all svgs have been removed");
+        }
+    }
+    else {
+        assert(d3.select("body").selectAll("svg").node() === null, "all svgs have been removed");
+    }
 });
 
 var assert = chai.assert;
