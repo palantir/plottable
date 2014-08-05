dfe47d9d0f871a6ec388d87a428d2746e2ca1736
Brandon Luong <bluong@palantir.com>
2014-08-05 15:01:43 -0700

    730 -Clear html in label's textContainer on render
    Closes #730.

--- plottable.js
+++ plottable.js
@@ -4021,7 +4021,7 @@ var Plottable;
             };
             Label.prototype._doRender = function () {
                 _super.prototype._doRender.call(this);
-                this.textContainer.selectAll("text").remove();
+                this.textContainer.html("");
                 var dimension = this.orientation === "horizontal" ? this.availableWidth : this.availableHeight;
                 var truncatedText = Plottable.Util.Text.getTruncatedText(this._text, dimension, this.measurer);
                 if (this.orientation === "horizontal") {
