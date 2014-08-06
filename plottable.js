279846126aefcbcd01239b27670428e16e62dca8
Brian Malehorn <bmalehorn@gmail.com>
2014-08-06 15:21:48 -0700

    domainer accounts for scale.domain() === [x, x]

--- plottable.js
+++ plottable.js
@@ -2407,6 +2407,9 @@ var Plottable;
                     return [d - Domainer.PADDING_FOR_IDENTICAL_DOMAIN, d + Domainer.PADDING_FOR_IDENTICAL_DOMAIN];
                 }
             }
+            if (scale.domain()[0] === scale.domain()[1]) {
+                return domain;
+            }
             var p = this.padProportion / 2;
             var newMin = scale.invert(scale.scale(min) - (scale.scale(max) - scale.scale(min)) * p);
             var newMax = scale.invert(scale.scale(max) + (scale.scale(max) - scale.scale(min)) * p);
