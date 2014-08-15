64d35e7835e10d9ac7decae7c8eb8443b2c94960
Brian Malehorn <bmalehorn@gmail.com>
2014-08-15 16:37:20 -0700

    Core.RenderController.RenderPolicy.* tsdoc

--- src/core/renderPolicy.ts
+++ src/core/renderPolicy.ts
@@ -9,18 +9,31 @@ export module RenderController {
       render(): any;
     }
 
+    /**
+     * Never queue anything, render everything immediately. Useful for
+     * debugging, horrible for performance.
+     */
     export class Immediate implements IRenderPolicy {
       public render() {
         RenderController.flush();
       }
     }
 
+    /**
+     * The default way to render, which only tries to render every frame
+     * (usually, 1/60th of a second).
+     */
     export class AnimationFrame implements IRenderPolicy {
       public render() {
         _Util.DOM.requestAnimationFramePolyfill(RenderController.flush);
       }
     }
 
+    /**
+     * Renders with `setTimeout`. This is generally an inferior way to render
+     * compared to `requestAnimationFrame`, but it's still there if you want
+     * it.
+     */
     export class Timeout implements IRenderPolicy {
       public _timeoutMsec: number = _Util.DOM.POLYFILL_TIMEOUT_MSEC;
 
