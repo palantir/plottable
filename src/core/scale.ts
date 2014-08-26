74fed25941365ce0d0e686141a5552f46c576dc8
Daniel Mane <danmane@gmail.com>
2014-08-26 12:34:22 -0700

    [minor] Fix parameter order

--- src/core/scale.ts
+++ src/core/scale.ts
@@ -48,7 +48,7 @@ export module Abstract {
     /**
      * Returns the range value corresponding to a given domain value.
      *
-     * @param value {D} A domain value to be scaled.
+     * @param {D} value A domain value to be scaled.
      * @returns {D} The range value corresponding to the supplied domain value.
      */
     public scale(value: D): R {
