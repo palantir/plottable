af43e53e57652b6a349f26258a63fcf293009665
Brandon Luong <bluong@palantir.com>
2014-08-28 14:45:52 -0700

    922 - fill starts off as right color before transition
    Massive refactor of animator as well as some edge case fixing
    Also a precommit for #918
    
    Close #922.

--- plottable-dev.d.ts
+++ plottable-dev.d.ts
@@ -1152,10 +1152,14 @@ declare module Plottable {
 declare module Plottable {
     module Animator {
         class Rect extends Default {
+            static ANIMATED_ATTRIBUTES: string[];
             isVertical: boolean;
             isReverse: boolean;
             constructor(isVertical?: boolean, isReverse?: boolean);
             animate(selection: any, attrToProjector: IAttributeToProjector): any;
+            _startMovingAttrProjector(attrToProjector: IAttributeToProjector): IAppliedAccessor;
+            _getGrowingAttr(): string;
+            _getMovingAttr(): string;
         }
     }
 }
