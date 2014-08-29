a6017dd50e9eb9c4eda909b223380fb295169df1
Daniel Mane <danmane@gmail.com>
2014-08-29 14:40:06 -0700

    Take quicktests from develop wholesale

---
+++ quicktests/js/animate_area.js
@@ -0,0 +1,37 @@
+function makeData() {
+  "use strict";
+
+  return [makeRandomData(50), makeRandomData(50)];
+}
+
+function run(div, data, Plottable) {
+  "use strict";
+
+  var svg = div.append("svg").attr("height", 500);
+
+  var doAnimate = true;
+
+  var xScale = new Plottable.Scale.Linear();
+  var xAxis = new Plottable.Axis.Numeric(xScale, "bottom");
+
+  var yScale = new Plottable.Scale.Linear();
+  var yAxis = new Plottable.Axis.Numeric(yScale, "left");
+
+  var areaRenderer = new Plottable.Plot.Area(data[0].slice(0, 20), xScale, yScale);
+  areaRenderer.project("opacity", 0.75);
+  areaRenderer.animate(doAnimate);
+
+  var areaChart = new Plottable.Component.Table([[yAxis, areaRenderer],
+   [null,  xAxis]]);
+  
+  areaChart.renderTo(svg);
+
+  var cb = function(x, y){
+    d = areaRenderer.dataSource().data();
+    areaRenderer.dataSource().data(d);
+  };
+
+  new Plottable.Interaction.Click(areaRenderer)
+    .callback(cb)
+    .registerWithComponent();
+}
