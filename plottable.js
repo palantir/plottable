789bc759c21176ddef61717c2ae1aed027856754
Justin Lan <jlan@palantir.com>
2014-09-10 14:12:02 -0700

    Update version # in plottable.js

--- plottable.js
+++ plottable.js
@@ -1,5 +1,5 @@
 /*!
-Plottable 0.28.0 (https://github.com/palantir/plottable)
+Plottable 0.28.1 (https://github.com/palantir/plottable)
 Copyright 2014 Palantir Technologies
 Licensed under MIT (https://github.com/palantir/plottable/blob/master/LICENSE)
 */
@@ -935,7 +935,7 @@ var Plottable;
 
 var Plottable;
 (function (Plottable) {
-    Plottable.version = "0.28.0";
+    Plottable.version = "0.28.1";
 })(Plottable || (Plottable = {}));
 
 var Plottable;
