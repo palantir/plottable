16fd721e753195518c702dba5d8afe3ab3b4345e
Andrzej Skrodzki <askrodzki@palantir.com>
2014-09-25 12:06:00 -0700

    Update test assertion message.

--- test/components/labelTests.ts
+++ test/components/labelTests.ts
@@ -117,13 +117,13 @@ describe("Labels", () => {
     var content = label._content;
     var text = content.select("text");
     var bbox = Plottable._Util.DOM.getBBox(text);
-    assert.closeTo(bbox.height, label.height(), 1, "label is not in horizontal position");
+    assert.closeTo(bbox.height, label.height(), 1, "label is in horizontal position");
 
     label.orient("vertical-right");
     text = content.select("text");
     bbox = Plottable._Util.DOM.getBBox(text);
     assertBBoxInclusion(label._element.select(".bounding-box"), text);
-    assert.closeTo(bbox.height, label.width(), window.Pixel_CloseTo_Requirement, "label is not rotated");
+    assert.closeTo(bbox.height, label.width(), window.Pixel_CloseTo_Requirement, "label is in vertical position");
 
     svg.remove();
   });
