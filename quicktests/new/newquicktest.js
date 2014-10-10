cb1a198f31a88ce918bc686cddcca901572eb918
tkimcoop <tkim@palantir.com>
2014-10-10 02:43:14 -0700

    Oct.10 3AM Progress

--- quicktests/new/newquicktest.js
+++ quicktests/new/newquicktest.js
@@ -1,15 +1,19 @@
+var plottableBranches=[];
+var first = true; //boolean to set order
+
 function renderPlots(){
-  clearTests();
-	var dropdown = $("#category")[0];
+  first = true;
+  var dropdown = $("#category")[0];
   var branch1 = $("#branch1")[0].value;
   var branch2 = $("#branch2")[0].value;
-	var category = dropdown.options[dropdown.selectedIndex].value;
-  loadQuickTests(category, branch1, branch2);
+  var category = dropdown.options[dropdown.selectedIndex].value;
+
+  clearTests();
+  pushBranchToArray(category, branch1);
+  pushBranchToArray(category, branch2);
 }
 
-function loadQuickTests(category, branch1, branch2){
-  var first = "first";
-  var second = "second";
+function loadQuickTests(category, branch){
   var qtestnames = []; //array of quicktest names
   //filter list of quicktests to list of quicktest names to pass to doSomething
   d3.json("list_of_quicktests.json", function (data){
@@ -23,19 +27,18 @@ function loadQuickTests(category, branch1, branch2){
       }
 
     });
-    loadQuickTestsInCategory(quickTestNames, category, branch1, first);
-    loadQuickTestsInCategory(quickTestNames, category, branch2, second);
+    loadQuickTestsInCategory(quickTestNames, category, branch, first);
+    first = false;
   });
 }
 
 function loadQuickTestsInCategory(quickTestNames, category, branchName, order){
   //x here is an array of test names
 
-  var plottableBranches=[];
   var branchName = branchName; //this needs to be retrieved from before
   var div;
 
-  if(order === "first"){
+  if(order){
     //div = $("#result1");
     div = d3.select("#result1");
   }
@@ -60,7 +63,7 @@ function loadQuickTestsInCategory(quickTestNames, category, branchName, order){
       var className = "quicktest " + name;
       //div.append("<div id='" + name + "'> </div>"); //create specific div for a specific quicktest
       var qtdiv = div.append("div").attr("class", className).append("div");
-      prepareQuickTest(qtdiv, result, branchName, plottableBranches);
+        runQuickTest(qtdiv, result, branchName, plottableBranches);
     });
   });//forEach
     
@@ -68,8 +71,7 @@ function loadQuickTestsInCategory(quickTestNames, category, branchName, order){
 
 //METHODS
 
-//retrieves the plottable object according to branch, then run
-function prepareQuickTest(div, result, branchName, plottableBranches) {
+function pushBranchToArray(category, branchName){
   if (plottableBranches[branchName] != null) {
     return;
   }
@@ -84,13 +86,19 @@ function prepareQuickTest(div, result, branchName, plottableBranches) {
     if(textStatus === "success"){
       console.log("success!")
       plottableBranches[branchName] = Plottable;
-      runQuickTest(div, result, branchName, plottableBranches); //run the quicktest once the plottable object is retrieved
+      loadQuickTests(category, branchName);
     }
     if(textStatus === "error"){
       console.log("errored!")
     }
 
   });
+}
+//retrieves the plottable object according to branch, then run
+function prepareQuickTest(div, result, branchName, plottableBranches) {
+        runQuickTest(div, result, branchName, plottableBranches); //run the quicktest once the plottable object is retrieved
+
+
 }//appendQuickTestBranch
 
 //run a single quicktest
@@ -100,30 +108,37 @@ function runQuickTest(div, result, branch, plottableBranches){
 
 
 function clearTests(){
+  plottableBranches = [];
+  resetDisplayProperties();
   d3.selectAll(".quicktest").remove();
 }
 
+function resetDisplayProperties(){
+  $("#result1, #result1 svg").css("display", "block");
+  $("#result2, #result2 svg").css("display", "block");
+
+}
 window.onkeyup = function(e){
   var key = e.keyCode ? e.keyCode : e.which;
   //if 1 is pressed
   if(key == 49){
-    $("#result1 svg").css("display", "block");
-    $("#result2 svg").css("display", "none");
+    $("#result1, #result1 svg").css("display", "block");
+    $("#result2, #result2 svg").css("display", "none");
   }
   //if 2 is pressed
   if(key == 50){
-    $("#result1 svg").css("display", "none");
-    $("#result2 svg").css("display", "block");
+    $("#result1, #result1 svg").css("display", "none");
+    $("#result2, #result2 svg").css("display", "block");
   }
   //if 3 is pressed
   if(key == 51){
-    $("#result1 svg").css("display", "block");
-    $("#result2 svg").css("display", "block");
+    $("#result1, #result1 svg").css("display", "block");
+    $("#result2, #result2 svg").css("display", "block");
   }
-    //if 3 is pressed
+  //if 4 is pressed
   if(key == 52){
-    $("#result1 svg").css("display", "none");
-    $("#result2 svg").css("display", "none");
+    $("#result1, #result1 svg").css("display", "none");
+    $("#result2, #result2 svg").css("display", "none");
   }
 }
 
