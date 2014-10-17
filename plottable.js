bb0fc9ef6a9a594e8940dd5fcfc7865e32ead60b
Brandon Luong <bluong@palantir.com>
2014-10-17 15:41:01 -0700

    Setting default bar alignment to 0.5 instead of 0

--- plottable.js
+++ plottable.js
@@ -6426,7 +6426,7 @@ var Plottable;
             function AbstractBarPlot(xScale, yScale) {
                 _super.call(this, xScale, yScale);
                 this._baselineValue = 0;
-                this._barAlignmentFactor = 0;
+                this._barAlignmentFactor = 0.5;
                 this._animators = {
                     "bars-reset": new Plottable.Animator.Null(),
                     "bars": new Plottable.Animator.Base(),
@@ -7290,7 +7290,6 @@ var Plottable;
                 if (isVertical === void 0) { isVertical = true; }
                 this._isVertical = isVertical; // Has to be set before super()
                 this._baselineValue = 0;
-                this._barAlignmentFactor = 0.5;
                 _super.call(this, xScale, yScale);
                 this.classed("bar-plot", true);
                 this.project("fill", function () { return Plottable.Core.Colors.INDIGO; });
