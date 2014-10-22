8a6313d4e3717749552ee3968c487bf10cb9b97e
tkimcoop <tkim@palantir.com>
2014-10-21 17:49:56 -0700

    some color changes

--- quicktests/new/newquicktest.js
+++ quicktests/new/newquicktest.js
@@ -134,29 +134,42 @@ function clearTests(){
 function resetDisplayProperties(){
   $("#result1, #result1 svg").css("display", "block");
   $("#result2, #result2 svg").css("display", "block");
-
+  $("#branch1").css("background-color", "white");
+  $("#branch2").css("background-color", "white");
 }
+
 window.onkeyup = function(e){
   var key = e.keyCode ? e.keyCode : e.which;
   //if 1 is pressed
   if(key == 49){
     $(".first svg").css("display", "block");
     $(".second svg").css("display", "none");
+    $("#branch1").css("background-color", "mediumaquamarine");
+    $("#branch2").css("background-color", "white");
   }
   //if 2 is pressed
   if(key == 50){
     $(".first svg").css("display", "none");
     $(".second svg").css("display", "block");
+    $("#branch1").css("background-color", "white");
+    $("#branch2").css("background-color", "mediumaquamarine");
+
   }
   //if 3 is pressed
   if(key == 51){
     $(".first svg").css("display", "block");
     $(".second svg").css("display", "block");
+    $("#branch1").css("background-color", "mediumaquamarine");
+    $("#branch2").css("background-color", "mediumaquamarine");
+
   }
   //if 4 is pressed
   if(key == 52){
     $(".first svg").css("display", "none");
     $(".second svg").css("display", "none");
+    $("#branch1").css("background-color", "white");
+    $("#branch2").css("background-color", "white");
+
   }
 }
 
