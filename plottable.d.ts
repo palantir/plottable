f2dd083d78b0ee7ef14abbf16d55f36f63fa6614
Brandon Luong <bluong@palantir.com>
2014-12-01 13:29:01 -0800

    dragbox vars private

--- plottable.d.ts
+++ plottable.d.ts
@@ -3806,9 +3806,6 @@ declare module Plottable {
              * null.
              */
             dragBox: D3.Selection;
-            _boxIsDrawn: boolean;
-            _resizeXEnabled: boolean;
-            _resizeYEnabled: boolean;
             /**
              * Gets whether resizing is enabled or not.
              *
