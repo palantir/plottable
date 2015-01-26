5fd9d6bdd1d94c33a882db8d98992aa34b362836
Cassie Morford <cmorford@palantir.com>
2015-01-26 10:32:44 -0800

    lint

--- quicktests/overlaying/tests/color/interpolatedScatter.js
+++ quicktests/overlaying/tests/color/interpolatedScatter.js
@@ -8,15 +8,15 @@ function run(svg, data, Plottable) {
   "use strict";
 
     var data = [
-        {x: "A", y: 1, val: .1},
-        {x: "B", y: 1, val: .2},
-        {x: "C", y: 1, val: .3},
-        {x: "D", y: 1, val: .4},
-        {x: "E", y: 1, val: .5},
-        {x: "A", y: 2, val: .6},
-        {x: "B", y: 2, val: .7},
-        {x: "C", y: 2, val: .8},
-        {x: "D", y: 2, val: .9},
+        {x: "A", y: 1, val: 0.1},
+        {x: "B", y: 1, val: 0.2},
+        {x: "C", y: 1, val: 0.3},
+        {x: "D", y: 1, val: 0.4},
+        {x: "E", y: 1, val: 0.5},
+        {x: "A", y: 2, val: 0.6},
+        {x: "B", y: 2, val: 0.7},
+        {x: "C", y: 2, val: 0.8},
+        {x: "D", y: 2, val: 0.9},
         {x: "E", y: 2, val: 1.0},
     ];
     var xScale = new Plottable.Scale.Ordinal();
@@ -28,7 +28,7 @@ function run(svg, data, Plottable) {
     plot.addDataset(data);
     plot.project("x", "x", xScale).project("y", "y", yScale);
     plot.project("r", function(){ return 10;});
-    plot.project("fill", function(d) { return d["val"]; }, colorScale);
+    plot.project("fill", function(d) { return d.val; }, colorScale);
     
     plot.renderTo(svg);
 
