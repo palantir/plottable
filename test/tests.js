a3789b6848af826d9b8e463ec617bf077829b9ba
Roger Chen <rchen@palantir.com>
2015-01-30 17:32:51 -0800

    [Tests] Consolidated Legend-related tests

--- test/tests.js
+++ test/tests.js
@@ -1167,20 +1167,22 @@ describe("Labels", function () {
 
 ///<reference path="../testReference.ts" />
 var assert = chai.assert;
-describe("Legends", function () {
+describe("Legend", function () {
     var svg;
     var color;
     var legend;
+    var entrySelector = "." + Plottable.Component.Legend.LEGEND_ENTRY_CLASS;
+    var rowSelector = "." + Plottable.Component.Legend.LEGEND_ROW_CLASS;
     beforeEach(function () {
         svg = generateSVG(400, 400);
-        color = new Plottable.Scale.Color("Category10");
-        legend = new Plottable.Component.Legend(color).maxEntriesPerRow(1);
+        color = new Plottable.Scale.Color();
+        legend = new Plottable.Component.Legend(color);
     });
     it("a basic legend renders", function () {
         color.domain(["foo", "bar", "baz"]);
         legend.renderTo(svg);
-        var rows = legend._content.selectAll(".legend-entry");
-        assert.lengthOf(rows[0], 3, "there are 3 legend entries");
+        var rows = legend._content.selectAll(entrySelector);
+        assert.lengthOf(rows[0], color.domain().length, "one entry is created for each item in the domain");
         rows.each(function (d, i) {
             assert.equal(d, color.domain()[i], "the data is set properly");
             var d3this = d3.select(this);
@@ -1203,7 +1205,7 @@ describe("Legends", function () {
         assert.operator(legend._requestedSpace(400, 400).height, ">", height1, "adding to the domain increases the height requested");
         var actualHeight2 = legend.height();
         assert.operator(actualHeight1, "<", actualHeight2, "Changing the domain caused the legend to re-layout with more height");
-        var numRows = legend._content.selectAll(".legend-row")[0].length;
+        var numRows = legend._content.selectAll(rowSelector)[0].length;
         assert.equal(numRows, 3, "there are 3 rows");
         svg.remove();
     });
@@ -1233,10 +1235,10 @@ describe("Legends", function () {
     it("calling legend.render multiple times does not add more elements", function () {
         color.domain(["foo", "bar", "baz"]);
         legend.renderTo(svg);
-        var numRows = legend._content.selectAll(".legend-row")[0].length;
+        var numRows = legend._content.selectAll(rowSelector)[0].length;
         assert.equal(numRows, 3, "there are 3 legend rows initially");
         legend._render();
-        numRows = legend._content.selectAll(".legend-row")[0].length;
+        numRows = legend._content.selectAll(rowSelector)[0].length;
         assert.equal(numRows, 3, "there are 3 legend rows after second render");
         svg.remove();
     });
@@ -1245,14 +1247,14 @@ describe("Legends", function () {
         legend.renderTo(svg);
         var newDomain = ["mushu", "foo", "persei", "baz", "eight"];
         color.domain(newDomain);
-        legend._content.selectAll(".legend-entry").each(function (d, i) {
+        legend._content.selectAll(entrySelector).each(function (d, i) {
             assert.equal(d, newDomain[i], "the data is set correctly");
             var text = d3.select(this).select("text").text();
             assert.equal(text, d, "the text was set properly");
             var fill = d3.select(this).select("circle").attr("fill");
             assert.equal(fill, color.scale(d), "the fill was set properly");
         });
-        assert.lengthOf(legend._content.selectAll(".legend-row")[0], 5, "there are the right number of legend elements");
+        assert.lengthOf(legend._content.selectAll(rowSelector)[0], 5, "there are the right number of legend elements");
         svg.remove();
     });
     it("legend.scale() replaces domain", function () {
@@ -1262,7 +1264,7 @@ describe("Legends", function () {
         var newColorScale = new Plottable.Scale.Color("20");
         newColorScale.domain(newDomain);
         legend.scale(newColorScale);
-        legend._content.selectAll(".legend-entry").each(function (d, i) {
+        legend._content.selectAll(entrySelector).each(function (d, i) {
             assert.equal(d, newDomain[i], "the data is set correctly");
             var text = d3.select(this).select("text").text();
             assert.equal(text, d, "the text was set properly");
@@ -1280,7 +1282,7 @@ describe("Legends", function () {
         legend.scale(newColorScale);
         var newDomain = ["a", "foo", "d"];
         newColorScale.domain(newDomain);
-        legend._content.selectAll(".legend-entry").each(function (d, i) {
+        legend._content.selectAll(entrySelector).each(function (d, i) {
             assert.equal(d, newDomain[i], "the data is set correctly");
             var text = d3.select(this).select("text").text();
             assert.equal(text, d, "the text was set properly");
@@ -1289,7 +1291,7 @@ describe("Legends", function () {
         });
         svg.remove();
     });
-    it("iconHeight / 2 < circleHeight < iconHeight", function () {
+    it("scales icon sizes properly with font size (iconHeight / 2 < circleHeight < iconHeight)", function () {
         color.domain(["foo"]);
         legend.renderTo(svg);
         var style = legend._element.append("style");
@@ -1313,118 +1315,12 @@ describe("Legends", function () {
         verifyCircleHeight();
         svg.remove();
     });
-});
-describe("Legend", function () {
-    var colorScale;
-    var horizLegend;
-    var entrySelector = "." + Plottable.Component.Legend.LEGEND_ENTRY_CLASS;
-    var rowSelector = "." + Plottable.Component.Legend.LEGEND_ROW_CLASS;
-    beforeEach(function () {
-        colorScale = new Plottable.Scale.Color();
-        colorScale.domain([
-            "Washington",
-            "Adams",
-            "Jefferson",
-        ]);
-        horizLegend = new Plottable.Component.Legend(colorScale).maxEntriesPerRow(Infinity);
-    });
-    it("renders an entry for each item in the domain", function () {
-        var svg = generateSVG(400, 100);
-        horizLegend.renderTo(svg);
-        var entries = horizLegend._element.selectAll(entrySelector);
-        assert.equal(entries[0].length, colorScale.domain().length, "one entry is created for each item in the domain");
-        var elementTexts = entries.select("text")[0].map(function (node) { return d3.select(node).text(); });
-        assert.deepEqual(elementTexts, colorScale.domain(), "entry texts match scale domain");
-        var newDomain = colorScale.domain();
-        newDomain.push("Madison");
-        colorScale.domain(newDomain);
-        entries = horizLegend._element.selectAll(entrySelector);
-        assert.equal(entries[0].length, colorScale.domain().length, "Legend updated to include item added to the domain");
-        svg.remove();
-    });
-    it("wraps entries onto extra rows if necessary", function () {
-        var svg = generateSVG(200, 100);
-        horizLegend.renderTo(svg);
-        var rows = horizLegend._element.selectAll(rowSelector);
-        assert.lengthOf(rows[0], 2, "Wrapped text on to two rows when space is constrained");
-        horizLegend.detach();
-        svg.remove();
-        svg = generateSVG(100, 100);
-        horizLegend.renderTo(svg);
-        rows = horizLegend._element.selectAll(rowSelector);
-        assert.lengthOf(rows[0], 3, "Wrapped text on to three rows when further constrained");
-        svg.remove();
-    });
-    it("truncates and hides entries if space is constrained", function () {
-        var svg = generateSVG(70, 400);
-        horizLegend.renderTo(svg);
-        var textEls = horizLegend._element.selectAll("text");
-        textEls.each(function (d) {
-            var textEl = d3.select(this);
-            assertBBoxInclusion(horizLegend._element, textEl);
-        });
-        horizLegend.detach();
-        svg.remove();
-        svg = generateSVG(100, 50);
-        horizLegend.renderTo(svg);
-        textEls = horizLegend._element.selectAll("text");
-        textEls.each(function (d) {
-            var textEl = d3.select(this);
-            assertBBoxInclusion(horizLegend._element, textEl);
-        });
-        svg.remove();
-    });
-    it("scales icon size with entry font size", function () {
-        colorScale.domain(["A"]);
-        var svg = generateSVG(400, 100);
-        horizLegend.renderTo(svg);
-        var style = horizLegend._element.append("style");
-        style.attr("type", "text/css");
-        function verifyCircleHeight() {
-            var text = horizLegend._content.select("text");
-            var circle = horizLegend._content.select("circle");
-            var textHeight = Plottable._Util.DOM.getBBox(text).height;
-            var circleHeight = Plottable._Util.DOM.getBBox(circle).height;
-            assert.operator(circleHeight, "<", textHeight, "Icon is smaller than entry text");
-            return circleHeight;
-        }
-        var origCircleHeight = verifyCircleHeight();
-        style.text(".plottable .legend text { font-size: 30px; }");
-        horizLegend._invalidateLayout();
-        var bigCircleHeight = verifyCircleHeight();
-        assert.operator(bigCircleHeight, ">", origCircleHeight, "icon size increased with font size");
-        style.text(".plottable .legend text { font-size: 6px; }");
-        horizLegend._invalidateLayout();
-        var smallCircleHeight = verifyCircleHeight();
-        assert.operator(smallCircleHeight, "<", origCircleHeight, "icon size decreased with font size");
-        svg.remove();
-    });
-    it("getEntry() horizontal", function () {
-        colorScale.domain(["AA", "BB", "CC"]);
-        var svg = generateSVG(300, 300);
-        horizLegend.renderTo(svg);
-        assert.deepEqual(horizLegend.getEntry({ x: 10, y: 10 }).data(), ["AA"], "get first entry");
-        assert.deepEqual(horizLegend.getEntry({ x: 50, y: 10 }).data(), ["BB"], "get second entry");
-        assert.deepEqual(horizLegend.getEntry({ x: 150, y: 10 }), d3.select(), "no entries at location outside legend");
-        svg.remove();
-    });
-    it("getEntry() vertical", function () {
-        colorScale.domain(["AA", "BB", "CC"]);
-        var svg = generateSVG(300, 300);
-        horizLegend.maxEntriesPerRow(1);
-        horizLegend.renderTo(svg);
-        assert.deepEqual(horizLegend.getEntry({ x: 10, y: 10 }).data(), ["AA"], "get first entry");
-        assert.deepEqual(horizLegend.getEntry({ x: 10, y: 30 }).data(), ["BB"], "get second entry");
-        assert.deepEqual(horizLegend.getEntry({ x: 10, y: 150 }), d3.select(), "no entries at location outside legend");
-        svg.remove();
-    });
     it("maxEntriesPerRow() works as expected", function () {
-        colorScale.domain(["AA", "BB", "CC", "DD", "EE", "FF"]);
-        var svg = generateSVG(300, 300);
-        horizLegend.renderTo(svg);
+        color.domain(["AA", "BB", "CC", "DD", "EE", "FF"]);
+        legend.renderTo(svg);
         var verifyMaxEntriesInRow = function (n) {
-            horizLegend.maxEntriesPerRow(n);
-            var rows = horizLegend._element.selectAll(rowSelector);
+            legend.maxEntriesPerRow(n);
+            var rows = legend._element.selectAll(rowSelector);
             assert.lengthOf(rows[0], (6 / n), "number of rows is correct");
             rows.each(function (d) {
                 var entries = d3.select(this).selectAll(entrySelector);
@@ -1437,20 +1333,72 @@ describe("Legend", function () {
         verifyMaxEntriesInRow(6);
         svg.remove();
     });
+    it("wraps entries onto extra rows if necessary for horizontal legends", function () {
+        color.domain(["George Waaaaaashington", "John Adaaaams", "Thomaaaaas Jefferson"]);
+        legend.maxEntriesPerRow(Infinity);
+        legend.renderTo(svg);
+        var rows = legend._element.selectAll(rowSelector);
+        assert.lengthOf(rows[0], 2, "Wrapped text on to two rows when space is constrained");
+        legend.detach();
+        svg.remove();
+        svg = generateSVG(100, 100);
+        legend.renderTo(svg);
+        rows = legend._element.selectAll(rowSelector);
+        assert.lengthOf(rows[0], 3, "Wrapped text on to three rows when further constrained");
+        svg.remove();
+    });
+    it("getEntry() retrieves the correct entry for vertical legends", function () {
+        color.domain(["AA", "BB", "CC"]);
+        legend.maxEntriesPerRow(1);
+        legend.renderTo(svg);
+        assert.deepEqual(legend.getEntry({ x: 10, y: 10 }).data(), ["AA"], "get first entry");
+        assert.deepEqual(legend.getEntry({ x: 10, y: 30 }).data(), ["BB"], "get second entry");
+        assert.deepEqual(legend.getEntry({ x: 10, y: 150 }), d3.select(), "no entries at location outside legend");
+        svg.remove();
+    });
+    it("getEntry() retrieves the correct entry for horizontal legends", function () {
+        color.domain(["AA", "BB", "CC"]);
+        legend.maxEntriesPerRow(Infinity);
+        legend.renderTo(svg);
+        assert.deepEqual(legend.getEntry({ x: 10, y: 10 }).data(), ["AA"], "get first entry");
+        assert.deepEqual(legend.getEntry({ x: 50, y: 10 }).data(), ["BB"], "get second entry");
+        assert.deepEqual(legend.getEntry({ x: 150, y: 10 }), d3.select(), "no entries at location outside legend");
+        svg.remove();
+    });
     it("sortFunction() works as expected", function () {
         var newDomain = ["F", "E", "D", "C", "B", "A"];
-        colorScale.domain(newDomain);
-        var svg = generateSVG(300, 300);
-        horizLegend.renderTo(svg);
-        var entries = horizLegend._element.selectAll(entrySelector);
+        color.domain(newDomain);
+        legend.renderTo(svg);
+        var entries = legend._element.selectAll(entrySelector);
         var elementTexts = entries.select("text")[0].map(function (node) { return d3.select(node).text(); });
         assert.deepEqual(elementTexts, newDomain, "entry has not been sorted");
         var sortFn = function (a, b) { return a.localeCompare(b); };
-        horizLegend.sortFunction(sortFn);
-        entries = horizLegend._element.selectAll(entrySelector);
+        legend.sortFunction(sortFn);
+        entries = legend._element.selectAll(entrySelector);
         elementTexts = entries.select("text")[0].map(function (node) { return d3.select(node).text(); });
         newDomain.sort(sortFn);
-        assert.deepEqual(elementTexts, newDomain, "entry has been sorted alfabetically");
+        assert.deepEqual(elementTexts, newDomain, "entry has been sorted alphabetically");
+        svg.remove();
+    });
+    it("truncates and hides entries if space is constrained for a horizontal legend", function () {
+        svg.remove();
+        svg = generateSVG(70, 400);
+        legend.maxEntriesPerRow(Infinity);
+        legend.renderTo(svg);
+        var textEls = legend._element.selectAll("text");
+        textEls.each(function (d) {
+            var textEl = d3.select(this);
+            assertBBoxInclusion(legend._element, textEl);
+        });
+        legend.detach();
+        svg.remove();
+        svg = generateSVG(100, 50);
+        legend.renderTo(svg);
+        textEls = legend._element.selectAll("text");
+        textEls.each(function (d) {
+            var textEl = d3.select(this);
+            assertBBoxInclusion(legend._element, textEl);
+        });
         svg.remove();
     });
 });
