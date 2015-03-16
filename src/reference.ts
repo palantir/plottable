f0ab6254d0c48df5064b4ec2507a44f0932b5aaf
Brandon Luong <bluong@palantir.com>
2015-03-16 16:04:04 -0700

    add wheelDispatcher to the references

--- src/reference.ts
+++ src/reference.ts
@@ -83,6 +83,7 @@
 /// <reference path="dispatchers/mouseDispatcher.ts" />
 /// <reference path="dispatchers/touchDispatcher.ts" />
 /// <reference path="dispatchers/keyDispatcher.ts" />
+/// <reference path="dispatchers/wheelDispatcher.ts" />
 
 /// <reference path="interactions/abstractInteraction.ts" />
 /// <reference path="interactions/clickInteraction.ts" />
