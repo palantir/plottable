d09f1f8ce0442b6e06665e55960c7e8f88b685df
Justin Lan <jlan@palantir.com>
2015-03-25 18:53:10 -0700

    Use background() instead of content() as target for fake events
    More reliable, particularly if the Component's content doesn't occupy the entire cell.

--- test/interactions/interactionTests.ts
+++ test/interactions/interactionTests.ts
@@ -97,9 +97,9 @@ describe("Interactions", () => {
       ki.on(bCode, bCallback);
       component.registerInteraction(ki);
 
-      var $target = $(component.content().node());
+      var $target = $(component.background().node());
 
-      triggerFakeMouseEvent("mouseover", component.content(), 100, 100);
+      triggerFakeMouseEvent("mouseover", component.background(), 100, 100);
       $target.simulate("keydown", { keyCode: aCode });
       assert.isTrue(aCallbackCalled, "callback for \"a\" was called when \"a\" key was pressed");
       assert.isFalse(bCallbackCalled, "callback for \"b\" was not called when \"a\" key was pressed");
@@ -109,7 +109,7 @@ describe("Interactions", () => {
       assert.isFalse(aCallbackCalled, "callback for \"a\" was not called when \"b\" key was pressed");
       assert.isTrue(bCallbackCalled, "callback for \"b\" was called when \"b\" key was pressed");
 
-      triggerFakeMouseEvent("mouseout", component.content(), -100, -100);
+      triggerFakeMouseEvent("mouseout", component.background(), -100, -100);
       aCallbackCalled = false;
       $target.simulate("keydown", { keyCode: aCode });
       assert.isFalse(aCallbackCalled, "callback for \"a\" was not called when not moused over the Component");
