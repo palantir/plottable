dd86883a4ef6f020ddc37579f70efe295ff587a4
Brandon Luong <bluong@palantir.com>
2015-04-07 00:16:18 -0700

    leftBounds/rightBounds

--- src/utils/scaleDomainTransformers.ts
+++ src/utils/scaleDomainTransformers.ts
@@ -12,9 +12,23 @@ module Plottable {
      * @param {number} translateAmount The amount to translate
      * @returns {D[]} The translated domain
      */
-    export function translate<D>(scale: Scale.AbstractQuantitative<D>, translateAmount: number) {
+    export function translate<D>(scale: Scale.AbstractQuantitative<D>, translateAmount: number,
+                                 leftBounds: D[], rightBounds: D[]) {
       var translateTransform = (rangeValue: number) => scale.invert(rangeValue - translateAmount);
-      scale.domain(scale.range().map(translateTransform));
+      var translatedDomain = scale.range().map(translateTransform);
+      if (translatedDomain[0] < leftBounds[0]) {
+        translatedDomain[0] = leftBounds[0];
+      }
+      if (translatedDomain[0] > leftBounds[1]) {
+        translatedDomain[0] = leftBounds[1];
+      }
+      if (translatedDomain[1] < leftBounds[0]) {
+        translatedDomain[1] = rightBounds[0];
+      }
+      if (translatedDomain[1] > leftBounds[1]) {
+        translatedDomain[1] = rightBounds[1];
+      }
+      scale.domain(translatedDomain);
     }
 
     /**
