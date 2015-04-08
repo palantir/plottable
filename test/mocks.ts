102131e9d364dad3b7ac0f8e97e8bbaee576fdb9
Justin Lan <jlan@palantir.com>
2015-04-07 17:30:30 -0700

    Add Mocks module.
    Contains mock-objects for use in tests.

---
+++ test/mocks.ts
@@ -0,0 +1,25 @@
+///<reference path="testReference.ts" />
+
+module Mocks {
+  export class FixedSizeComponent extends Plottable.Component.AbstractComponent {
+    public fixedWidth: number;
+    public fixedHeight: number;
+
+    constructor(width = 0, height = 0) {
+      super();
+      this.fixedWidth = width;
+      this.fixedHeight = height;
+      this._fixedWidthFlag = true;
+      this._fixedHeightFlag = true;
+    }
+
+    public _requestedSpace(availableWidth : number, availableHeight: number): Plottable._SpaceRequest {
+      return {
+        width:  this.fixedWidth,
+        height: this.fixedHeight,
+        wantsWidth : availableWidth < this.fixedWidth,
+        wantsHeight: availableHeight < this.fixedHeight
+      };
+    }
+  }
+}
