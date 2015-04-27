9b4333488084f277e81851c843287843a1c1b447
Brandon Luong <bluong@palantir.com>
2015-04-27 13:09:38 -0700

    adding tests

--- test/tests.js
+++ test/tests.js
@@ -9617,6 +9617,36 @@ describe("Dispatchers", function () {
             td.onTouchEnd(keyString, null);
             target.remove();
         });
+        it("onTouchCancel()", function () {
+            var targetWidth = 400, targetHeight = 400;
+            var target = generateSVG(targetWidth, targetHeight);
+            // HACKHACK: PhantomJS can't measure SVGs unless they have something in them occupying space
+            target.append("rect").attr("width", targetWidth).attr("height", targetHeight);
+            var targetXs = [17, 18, 12, 23, 44];
+            var targetYs = [77, 78, 52, 43, 14];
+            var expectedPoints = targetXs.map(function (targetX, i) {
+                return {
+                    x: targetX,
+                    y: targetYs[i]
+                };
+            });
+            var ids = targetXs.map(function (targetX, i) { return i; });
+            var td = Plottable.Dispatcher.Touch.getDispatcher(target.node());
+            var callbackWasCalled = false;
+            var callback = function (ids, points, e) {
+                callbackWasCalled = true;
+                ids.forEach(function (id) {
+                    assertPointsClose(points[id], expectedPoints[id], 0.5, "touch position is correct");
+                });
+                assert.isNotNull(e, "TouchEvent was passed to the Dispatcher");
+            };
+            var keyString = "unit test";
+            td.onTouchCancel(keyString, callback);
+            triggerFakeTouchEvent("touchcancel", target, expectedPoints, ids);
+            assert.isTrue(callbackWasCalled, "callback was called on touchend");
+            td.onTouchCancel(keyString, null);
+            target.remove();
+        });
         it("doesn't call callbacks if not in the DOM", function () {
             var targetWidth = 400, targetHeight = 400;
             var target = generateSVG(targetWidth, targetHeight);
