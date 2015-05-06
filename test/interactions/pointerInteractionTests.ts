b60fd41a4f9885713fe32c66e480310d9fbadaf9
Andrei Cioara <acioara@palantir.com>
2015-05-06 12:55:07 -0700

    [interactionsOff] Added multiple interaction test for pointer interaction

--- test/interactions/pointerInteractionTests.ts
+++ test/interactions/pointerInteractionTests.ts
@@ -156,5 +156,77 @@ describe("Interactions", () => {
 
       svg.remove();
     });
+
+    it("multiple callbacks can be added to pointer interaction", () => {
+      var svg = TestMethods.generateSVG(SVG_WIDTH, SVG_HEIGHT);
+      var component = new Plottable.Component();
+      component.renderTo(svg);
+
+      var pointer = new Plottable.Interactions.Pointer();
+      var enterCallback1Called = false;
+      var enterCallback2Called = false;
+      var moveCallback1Called = false;
+      var moveCallback2Called = false;
+      var exitCallback1Called = false;
+      var exitCallback2Called = false;
+
+      var enterCallback1 = () => enterCallback1Called = true;
+      var enterCallback2 = () => enterCallback2Called = true;
+      var moveCallback1 = () => moveCallback1Called = true;
+      var moveCallback2 = () => moveCallback2Called = true;
+      var exitCallback1 = () => exitCallback1Called = true;
+      var exitCallback2 = () => exitCallback2Called = true;
+
+      pointer.onPointerEnter(enterCallback1);
+      pointer.onPointerEnter(enterCallback2);
+      pointer.onPointerMove(moveCallback1);
+      pointer.onPointerMove(moveCallback2);
+      pointer.onPointerExit(exitCallback1);
+      pointer.onPointerExit(exitCallback2);
+
+      component.registerInteraction(pointer);
+
+      var target = component.background();
+      var insidePoint = { x: SVG_WIDTH / 2, y: SVG_HEIGHT / 2 };
+      var outsidePoint = { x: SVG_WIDTH * 2, y: SVG_HEIGHT * 2 };
+      TestMethods.triggerFakeMouseEvent("mousemove", target, outsidePoint.x, outsidePoint.y);
+      TestMethods.triggerFakeMouseEvent("mousemove", target, insidePoint.x, insidePoint.y);
+      TestMethods.triggerFakeMouseEvent("mousemove", target, outsidePoint.x, outsidePoint.y);
+
+      assert.isTrue(enterCallback1Called, "callback 1 was called on entering Component (mouse)");
+      assert.isTrue(enterCallback2Called, "callback 2 was called on entering Component (mouse)");
+
+      assert.isTrue(moveCallback1Called, "callback 1 was called on moving inside Component (mouse)");
+      assert.isTrue(moveCallback2Called, "callback 2 was called on moving inside Component (mouse)");
+
+      assert.isTrue(exitCallback1Called, "callback 1 was called on exiting Component (mouse)");
+      assert.isTrue(exitCallback2Called, "callback 2 was called on exiting Component (mouse)");
+
+      enterCallback1Called = false;
+      enterCallback2Called = false;
+      moveCallback1Called = false;
+      moveCallback2Called = false;
+      exitCallback1Called = false;
+      exitCallback2Called = false;
+
+      pointer.offPointerEnter(enterCallback1);
+      pointer.offPointerMove(moveCallback1);
+      pointer.offPointerExit(exitCallback1);
+
+      TestMethods.triggerFakeMouseEvent("mousemove", target, outsidePoint.x, outsidePoint.y);
+      TestMethods.triggerFakeMouseEvent("mousemove", target, insidePoint.x, insidePoint.y);
+      TestMethods.triggerFakeMouseEvent("mousemove", target, outsidePoint.x, outsidePoint.y);
+
+      assert.isFalse(enterCallback1Called, "callback 1 was disconnected from pointer enter interaction");
+      assert.isTrue(enterCallback2Called, "callback 2 is still connected to the pointer enter interaction");
+
+      assert.isFalse(moveCallback1Called, "callback 1 was disconnected from pointer interaction");
+      assert.isTrue(moveCallback2Called, "callback 2 is still connected to the pointer interaction");
+
+      assert.isFalse(exitCallback1Called, "callback 1 was disconnected from the pointer exit interaction");
+      assert.isTrue(exitCallback2Called, "callback 2 is still connected to the pointer exit interaction");
+
+      svg.remove();
+    });
   });
 });
