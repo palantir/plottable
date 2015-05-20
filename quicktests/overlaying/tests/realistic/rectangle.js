90c23ca06815d0fc206b0ae4e43d26f60bf6d84e
Roger Chen <rchen@palantir.com>
2015-05-20 13:59:35 -0700

    Updated quicktest

--- quicktests/overlaying/tests/realistic/rectangle.js
+++ quicktests/overlaying/tests/realistic/rectangle.js
@@ -1,31 +1,31 @@
 function makeData() {
   "use strict";
-  return [{team: "Detroit Tigers", x1: "4/1/1901", x2: "8/1/2015",
-                 y1: 0, y2: 1,
+  return [{team: "Detroit Tigers", x: "4/1/1901", x1: "8/1/2015",
+                 y: 0, y1: 1,
                  fill: "#DE4406", stroke: "#001742"},
 
-                {team: "Detroit Wolverines", x1: "4/1/1881", x2: "8/1/1888",
-                 y1: 1, y2: 2,
+                {team: "Detroit Wolverines", x: "4/1/1881", x1: "8/1/1888",
+                 y: 1, y1: 2,
                  fill: "#f5f5dc", stroke: "#CF0032"},
 
-                 {team: "Detroit Stars", x1: "4/1/1919", x2: "8/1/1931",
-                 y1: 2, y2: 3,
+                 {team: "Detroit Stars", x: "4/1/1919", x1: "8/1/1931",
+                 y: 2, y1: 3,
                  fill: "#00529B", stroke: "#CF0032"},
 
-                 {team: "Detroit Stars", x1: "4/1/1933", x2: "8/1/1933",
-                 y1: 2, y2: 3,
+                 {team: "Detroit Stars", x: "4/1/1933", x1: "8/1/1933",
+                 y: 2, y1: 3,
                  fill: "#00529B", stroke: "#CF0032"},
 
-                 {team: "Detroit Stars", x1: "4/1/1937", x2: "8/1/1937",
-                 y1: 2, y2: 3,
+                 {team: "Detroit Stars", x: "4/1/1937", x1: "8/1/1937",
+                 y: 2, y1: 3,
                  fill: "#00529B", stroke: "#CF0032"},
 
-                 {team: "Detroit Stars", x1: "4/1/1954", x2: "8/1/1957",
-                 y1: 2, y2: 3,
+                 {team: "Detroit Stars", x: "4/1/1954", x1: "8/1/1957",
+                 y: 2, y1: 3,
                  fill: "#00529B", stroke: "#CF0032"},
 
-                 {team: "Detroit Stars", x1: "4/1/1959", x2: "8/1/1959",
-                 y1: 2, y2: 3,
+                 {team: "Detroit Stars", x: "4/1/1959", x1: "8/1/1959",
+                 y: 2, y1: 3,
                  fill: "#00529B", stroke: "#CF0032"}
                ];
 }
@@ -33,19 +33,19 @@ function makeData() {
 function run(svg, data, Plottable) {
   "use strict";
 
-  var timeFormatStart = function (data) { return d3.time.format("%m/%d/%Y").parse(data.x1);};
-  var timeFormatEnd = function (data) { return d3.time.format("%m/%d/%Y").parse(data.x2);};
+  var timeFormatStart = function (data) { return d3.time.format("%m/%d/%Y").parse(data.x);};
+  var timeFormatEnd = function (data) { return d3.time.format("%m/%d/%Y").parse(data.x1);};
 
   var xScale = new Plottable.Scales.Time();
   var yScale = new Plottable.Scales.Category();
   yScale.innerPadding(0.25).outerPadding(0.25);
   var xAxis = new Plottable.Axes.Time(xScale, "bottom");
   var yAxis = new Plottable.Axes.Category(yScale, "left");
-  var plot = new Plottable.Plots.Grid(xScale, yScale);
+  var plot = new Plottable.Plots.Rectangle(xScale, yScale);
   plot.addDataset(new Plottable.Dataset(data));
   plot.x(timeFormatStart, xScale)
   .y(function(d) { return d.team; }, yScale)
-  .x2(timeFormatEnd, xScale)
+  .x1(timeFormatEnd, xScale)
   .attr("fill", function(d) { return d.fill; })
   .attr("stroke", function(d) { return d.stroke; });
 
