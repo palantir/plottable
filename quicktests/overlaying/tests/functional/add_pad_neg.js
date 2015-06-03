e0bb89e742007808237a1ac9d2a0bacd5f4e4e91
Cassie Morford <cmorford@palantir.com>
2015-06-03 10:47:01 -0700

    padding.js shows removeIVP issue, add_pad_neg.js shows component.detach error

--- quicktests/overlaying/tests/functional/add_pad_neg.js
+++ quicktests/overlaying/tests/functional/add_pad_neg.js
@@ -19,17 +19,17 @@ function run(svg, data, Plottable) {
   var bigVal = 10;
   var negVal = -10;  
 
-  function addBigVal() {
-    xScale.addIncludedValuesProvider(function() { return [bigVal]; });
-  }
-
-  function removeBigVal() {
-    xScale.removeIncludedValuesProvider(function() { return [bigVal]; });
-  }
-
-  // function addNegVal() {
-  //   xScale.addIncludedValuesProvider(function() { return [negVal]; });
+  // function addBigVal() {
+  //   xScale.addIncludedValuesProvider(function() { return [bigVal]; });
   // }
+
+  // function removeBigVal() {
+  //   xScale.removeIncludedValuesProvider(function() { return [bigVal]; });
+  // }
+
+  function addNegVal() {
+    xScale.addIncludedValuesProvider(function() { return [negVal]; });
+  }
 
   // function removeNegVal() {
   //   xScale.removeIncludedValuesProvider(function() { return [negVal]; });
@@ -39,9 +39,9 @@ function run(svg, data, Plottable) {
   //   xScale.addPaddingExceptionsProvider(function() { return [bigVal]; });
   // }
 
-  // function padExceptionNeg() {
-  //   xScale.addPaddingExceptionsProvider(function() { return [negVal]; });
-  // }
+  function padExceptionNeg() {
+    xScale.addPaddingExceptionsProvider(function() { return [negVal]; });
+  }
 
   // function removePadExpBig() {
   //   xScale.removePaddingExceptionsProvider(function() { return [bigVal]; });
@@ -53,18 +53,18 @@ function run(svg, data, Plottable) {
 
 
 
-  var addBig  = new Plottable.Components.Label("include big number", 0);
-  var removeBig  = new Plottable.Components.Label("remove big number", 0);
-  // var addNeg  = new Plottable.Components.Label("include neg number", 0);
+  // var addBig  = new Plottable.Components.Label("include big number", 0);
+  // var removeBig  = new Plottable.Components.Label("remove big number", 0);
+  var addNeg  = new Plottable.Components.Label("include neg number", 0);
   //var removeNeg  = new Plottable.Components.Label("remove neg number", 0);
   // var addPadExceptionBig  = new Plottable.Components.Label("padding exception big", 0);
-  // var addPadExceptionNeg  = new Plottable.Components.Label("padding exception neg", 0);
+  var addPadExceptionNeg  = new Plottable.Components.Label("padding exception neg", 0);
   //var removePadExceptionBig  = new Plottable.Components.Label("remove padding exception big", 0);
   //var removePadExceptionNeg  = new Plottable.Components.Label("remove padding exception neg", 0);  
 
-  var labelTable = new Plottable.Components.Table([[addBig, removeBig],
-                                                    // [addNeg, removeNeg],
-                                                    // [padExceptionBig, padExceptionNeg],
+  var labelTable = new Plottable.Components.Table([//[addBig, removeBig],
+                                                    [addNeg /*, removeNeg*/],
+                                                    [/*padExceptionBig,*/ padExceptionNeg],
                                                     //[removePadExceptionBig, removePadExceptionNeg]
                                                   ]);
 
@@ -77,12 +77,12 @@ function run(svg, data, Plottable) {
   basicTable.renderTo(svg);
 
 
-  new Plottable.Interactions.Click().onClick(addBigVal).attachTo(addBig);
-  new Plottable.Interactions.Click().onClick(removeBigVal).attachTo(removeBig);
-  // new Plottable.Interactions.Click().onClick(addNegVal).attachTo(addNeg);
+  // new Plottable.Interactions.Click().onClick(addBigVal).attachTo(addBig);
+  // new Plottable.Interactions.Click().onClick(removeBigVal).attachTo(removeBig);
+  new Plottable.Interactions.Click().onClick(addNegVal).attachTo(addNeg);
   //new Plottable.Interactions.Click().onClick(removeNegVal).attachTo(removeNeg);
   // new Plottable.Interactions.Click().onClick(padExceptionBig).attachTo(addPadExceptionBig);
-   // new Plottable.Interactions.Click().onClick(padExceptionNeg).attachTo(addPadExceptionNeg);
+   new Plottable.Interactions.Click().onClick(padExceptionNeg).attachTo(addPadExceptionNeg);
   //new Plottable.Interactions.Click().onClick(removePadExpBig).attachTo(removePadExceptionBig);
   //new Plottable.Interactions.Click().onClick(removePadExpNeg).attachTo(removePadExceptionNeg);
 
