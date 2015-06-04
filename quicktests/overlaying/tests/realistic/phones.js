1f4cdd6013bcdac3021e31530983dc3b01e92876
Cassie Morford <cmorford@palantir.com>
2015-06-04 14:38:22 -0700

    pushing to github for help figuring out why undefined shows up on each yaxis

--- quicktests/overlaying/tests/realistic/phones.js
+++ quicktests/overlaying/tests/realistic/phones.js
@@ -31,7 +31,23 @@ function makeData() {
     {phoneType: "smart",    demographic: "Ages 45-64",  value: 0.06}
   ];
 
-  return [dataAustralia, dataIndia];
+  var dataRoK = [
+    {phoneType: "feature",  demographic: "Male",        value: 0.20},
+    {phoneType: "feature",  demographic: "Female",      value: 0.25},
+    {phoneType: "feature",  demographic: "Ages 16-24",  value: 0.08},
+    {phoneType: "feature",  demographic: "Ages 25-34",  value: 0.07},
+    {phoneType: "feature",  demographic: "Ages 35-44",  value: 0.21},
+    {phoneType: "feature",  demographic: "Ages 45-64",  value: 0.38},
+    {phoneType: "smart",    demographic: "Male",        value: 0.68},
+    {phoneType: "smart",    demographic: "Female",      value: 0.65},
+    {phoneType: "smart",    demographic: "Ages 16-24",  value: 0.86},
+    {phoneType: "smart",    demographic: "Ages 25-34",  value: 0.86},
+    {phoneType: "smart",    demographic: "Ages 35-44",  value: 0.66},
+    {phoneType: "smart",    demographic: "Ages 45-64",  value: 0.49}
+  ];
+
+
+  return [dataAustralia, dataIndia, dataRoK];
 }
 
 function run(svg, data, Plottable) {
@@ -39,15 +55,33 @@ function run(svg, data, Plottable) {
 
   var AusColor = "#ff6969";
   var IndColor = "#662a48";
+  var RoKColor = "#cc3300"
+
+  var colorScale = new Plottable.Scales.Color();
+  colorScale.domain(["Australia", "India", "South Korea"]);
+  colorScale.range([AusColor, IndColor, RoKColor]);
+
+  var legend = new Plottable.Components.Legend(colorScale)
+    .maxEntriesPerRow(3)
+    .xAlignment("left");
+
   var AusDataset = new Plottable.Dataset(data[0]);
   var IndDataset = new Plottable.Dataset(data[1]);
+  var RoKDataset = new Plottable.Dataset(data[2]);
 
-  var xScale = new Plottable.Scales.Linear().domain([0, 1]);
+  var xScale = new Plottable.Scales.Linear().domain([0, 1])
   var xScale_reverse = new Plottable.Scales.Linear().domain([1, 0]);
   var yScale = new Plottable.Scales.Category();
 
-  var AusLabels = new Plottable.Axes.Category(yScale, "left");
-  var IndLabels = new Plottable.Axes.Category(yScale, "left");
+  var AusLabels = new Plottable.Axes.Category(yScale, "left")
+    .tickLength(0)
+    .endTickLength(0);
+  var IndLabels = new Plottable.Axes.Category(yScale, "left")
+    .tickLength(0)
+    .endTickLength(0);
+  var RoKLabels = new Plottable.Axes.Category(yScale, "left")
+    .tickLength(0)
+    .endTickLength(0);    
   var featureLabel = new Plottable.Components.Label("Feature Phones", 0);
   var smartLabel = new Plottable.Components.Label("Smart Phones", 0);
 
@@ -79,8 +113,8 @@ function run(svg, data, Plottable) {
 
   var AusSmartPlot = new Plottable.Plots.Bar("horizontal")
       .addDataset(AusDataset)
-      .x(filterFeatureX, xScale)
-      .y(filterFeatureY, yScale)
+      .x(filterSmartX, xScale)
+      .y(filterSmartY, yScale)
       .attr("fill", AusColor);
 
   var IndFeaturePlot = new Plottable.Plots.Bar("horizontal")
@@ -93,9 +127,24 @@ function run(svg, data, Plottable) {
       .addDataset(IndDataset)
       .x(filterSmartX, xScale)
       .y(filterSmartY, yScale)
-      .attr("fill", IndColor);          
+      .attr("fill", IndColor); 
+
+  var RoKFeaturePlot = new Plottable.Plots.Bar("horizontal")
+      .addDataset(RoKDataset)
+      .x(filterFeatureX, xScale_reverse)
+      .y(filterFeatureY, yScale)
+      .attr("fill", RoKColor);   
+
+  var RoKSmartPlot = new Plottable.Plots.Bar("horizontal")
+      .addDataset(RoKDataset)
+      .x(filterSmartX, xScale)
+      .y(filterSmartY, yScale)
+      .attr("fill", RoKColor);                
   
-  var table = new Plottable.Components.Table([[featureLabel,    null,       smartLabel],
+  var table = new Plottable.Components.Table([[legend,          null,       null],
+                                              [featureLabel,    null,       smartLabel],
                                               [AusFeaturePlot,  AusLabels,  AusSmartPlot],
-                                              [IndFeaturePlot,  IndLabels,  IndSmartPlot]]);
+                                              [IndFeaturePlot,  IndLabels,  IndSmartPlot],
+                                              [RoKFeaturePlot,  RoKLabels,  RoKSmartPlot]]);
+  table.renderTo(svg);
 }
