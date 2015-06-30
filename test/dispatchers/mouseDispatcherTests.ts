84ea4f83f14eb6aea9c9cae10ed14c432dcf268d
Sander Kromwijk <sanderk@palantir.com>
2015-06-29 21:09:12 -0400

    Implement test for click overlay

--- test/dispatchers/mouseDispatcherTests.ts
+++ test/dispatchers/mouseDispatcherTests.ts
@@ -86,6 +86,42 @@ describe("Dispatchers", () => {
       md.offMouseMove(callback);
     });
 
+    it("doesn't call callbacks for clicks if obscured by overlay", () => {
+      var targetWidth = 400, targetHeight = 400;
+      var target = TestMethods.generateSVG(targetWidth, targetHeight);
+      // HACKHACK: PhantomJS can't measure SVGs unless they have something in them occupying space
+      target.append("rect").attr("width", targetWidth).attr("height", targetHeight);
+
+      var targetX = 17;
+      var targetY = 76;
+
+      var md = Plottable.Dispatchers.Mouse.getDispatcher(<SVGElement> target.node());
+
+      var callbackWasCalled = false;
+      var callback = (p: Plottable.Point, e: MouseEvent) => callbackWasCalled = true;
+
+      md.onMouseDown(callback);
+      TestMethods.triggerFakeMouseEvent("mousedown", target, targetX, targetY);
+      assert.isTrue(callbackWasCalled, "callback was called on mousedown");
+
+      var overlay = TestMethods.getSVGParent().append("div")
+			.attr("style",
+			  "height: 400px;" +
+			  "width: 400px;" +
+			  "position: absolute;" +
+			  "top: 0;" +
+			  "left: 0"
+			);
+
+      callbackWasCalled = false;
+      TestMethods.triggerFakeMouseEvent("mousedown", overlay, targetX, targetY);
+      assert.isFalse(callbackWasCalled, "callback was not called on mousedown on overlay");
+
+      md.offMouseDown(callback);
+      target.remove();
+      overlay.remove();
+    });
+
     it("calls callbacks on mouseover, mousemove, and mouseout", () => {
       var targetWidth = 400, targetHeight = 400;
       var target = TestMethods.generateSVG(targetWidth, targetHeight);
