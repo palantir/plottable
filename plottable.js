015924cd84047d9b9dbb6e856521e7d95e60864c
Roger Chen <rchen@palantir.com>
2015-07-07 15:28:34 -0700

    Address comments, fix bug where plot did not work with ModifiedLog

--- plottable.js
+++ plottable.js
@@ -8613,7 +8613,7 @@ var Plottable;
                         else {
                             var currentSubtotal = _this._subtotals[i];
                             if (i === 0) {
-                                return Math.abs(yScale.scale(Math.abs(currentValue)) - yScale.scale(0));
+                                return Math.abs(yScale.scale(currentSubtotal) - yScale.scale(currentSubtotal - currentValue));
                             }
                             else {
                                 var priorSubtotal = _this._subtotals[i - 1];
@@ -8663,14 +8663,14 @@ var Plottable;
                         max = total;
                     }
                     if (startValue === -Infinity && isTotal) {
-                        var adjustment = currentValue - total;
+                        var startTotal = currentValue - total;
                         for (var i = 0; i < _this._subtotals.length; i++) {
-                            _this._subtotals[i] += adjustment;
+                            _this._subtotals[i] += startTotal;
                         }
                         startValue = _this._subtotals[i];
-                        total += adjustment;
-                        min += adjustment;
-                        max += adjustment;
+                        total += startTotal;
+                        min += startTotal;
+                        max += startTotal;
                     }
                 });
                 this._extent = [min, max];
