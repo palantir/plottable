b1950dbc3e0ef33de1552990cc19e4146693b055
Zoe Tsai <ztsai@palantir.com>
2015-07-20 15:54:20 -0700

    2106 Legend.symbol() doesn't pass in index correctly
    index was wrong due to nested selection

--- test/components/legendTests.ts
+++ test/components/legendTests.ts
@@ -355,4 +355,24 @@ describe("Legend", () => {
 
     svg.remove();
   });
+
+  it("symbol() passes index correctly", () => {
+    var domain = ["AA", "BB"];
+    color.domain(domain);
+    var squareFactory = Plottable.SymbolFactories.square();
+    var circleFactory = Plottable.SymbolFactories.circle();
+    var getFactory  = (i: number) => i === 0 ? squareFactory : circleFactory;
+
+    legend.symbol((d: any, i: number) => getFactory(i));
+    legend.renderTo(svg);
+
+    var symbols = d3.selectAll(symbolSelector);
+    symbols.each(function(d: any, i: number) {
+      var element = d3.select(this);
+      var symbolSize = this.getBoundingClientRect().height;
+      assert.strictEqual(element.attr("d"), getFactory(i)(symbolSize));
+    });
+
+    svg.remove();
+  });
 });
