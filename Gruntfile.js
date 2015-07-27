928be68e0aedcb3645c750513281f665b379aef7
Andrei Cioara <acioara@palantir.com>
2015-07-27 15:32:12 -0700

    [gruntSilent] Cleaned the comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -369,7 +369,7 @@ module.exports = function(grunt) {
   grunt.registerTask("test-sauce", ["connect", "saucelabs-mocha"]);
 
   grunt.registerTask("watch-silent", function() {
-    // Surpresses the "Running 'foo' task" messages. More info at #2507.
+    // Surpresses the "Running 'foo' task" messages
     grunt.log.header = function() {};
     grunt.task.run(["watch"]);
   });
