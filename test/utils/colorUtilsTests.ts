d5bcf869af7da4c7809ecdf2bc6e31b4252c1fa4
Brandon Luong <bluong@palantir.com>
2015-07-31 15:10:18 -0700

    colorUtilsTests ban var

--- test/utils/colorUtilsTests.ts
+++ test/utils/colorUtilsTests.ts
@@ -1,28 +1,27 @@
 ///<reference path="../testReference.ts" />
-/* tslint:disable: no-var-keyword */
 
 describe("Utils.Color", () => {
   it("lightenColor()", () => {
-    var colorHex = "#12fced";
-    var oldColor = d3.hsl(colorHex);
-    var lightenedColor = Plottable.Utils.Color.lightenColor(colorHex, 1);
+    const colorHex = "#12fced";
+    const oldColor = d3.hsl(colorHex);
+    const lightenedColor = Plottable.Utils.Color.lightenColor(colorHex, 1);
     assert.operator(d3.hsl(lightenedColor).l, ">", oldColor.l, "color got lighter");
   });
 
   it("colorTest()", () => {
-    var colorTester = d3.select("body").append("div").classed("color-tester", true);
-    var style = colorTester.append("style");
+    const colorTester = d3.select("body").append("div").classed("color-tester", true);
+    const style = colorTester.append("style");
     style.attr("type", "text/css");
 
     style.text(".plottable-colors-0 { background-color: blue; }");
-    var blueHexcode = Plottable.Utils.Color.colorTest(colorTester, "plottable-colors-0");
+    const blueHexcode = Plottable.Utils.Color.colorTest(colorTester, "plottable-colors-0");
     assert.strictEqual(blueHexcode, "#0000ff", "hexcode for blue returned");
 
     style.text(".plottable-colors-2 { background-color: #13EADF; }");
-    var hexcode = Plottable.Utils.Color.colorTest(colorTester, "plottable-colors-2");
+    const hexcode = Plottable.Utils.Color.colorTest(colorTester, "plottable-colors-2");
     assert.strictEqual(hexcode, "#13eadf", "hexcode for blue returned");
 
-    var nullHexcode = Plottable.Utils.Color.colorTest(colorTester, "plottable-colors-11");
+    const nullHexcode = Plottable.Utils.Color.colorTest(colorTester, "plottable-colors-11");
     assert.strictEqual(nullHexcode, null, "null hexcode returned");
     colorTester.remove();
   });
