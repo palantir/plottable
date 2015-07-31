34006b01f21e293ce327f99481cdae2dc97dc3a3
Brandon Luong <bluong@palantir.com>
2015-07-31 15:17:06 -0700

    arrayUtilsTests ban var

--- test/utils/arrayUtilsTests.ts
+++ test/utils/arrayUtilsTests.ts
@@ -1,11 +1,10 @@
 ///<reference path="../testReference.ts" />
-/* tslint:disable: no-var-keyword */
 
 describe("Utils", () => {
   describe("ArrayUtils", () => {
 
     it("uniq()", () => {
-      var strings = ["foo", "bar", "foo", "foo", "baz", "bam"];
+      const strings = ["foo", "bar", "foo", "foo", "baz", "bam"];
       assert.deepEqual(Plottable.Utils.Array.uniq(strings), ["foo", "bar", "baz", "bam"]);
     });
 
