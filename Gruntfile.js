23e132cc75eae96845b68d5d1787c4cec5ed91d8
Cassie <cmorford@palantir.com>
2015-09-03 16:30:18 -0700

    revert change to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -209,7 +209,6 @@ module.exports = function(grunt) {
     server: {
       options: {
         port: 9999,
-        protocol: "https",
         hostname: "*",
         base: "",
         livereload: true
