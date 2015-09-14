4144517efdc6ef0d8f79d80599dc60701c69a2f4
Andrei Cioara <acioara@palantir.com>
2015-09-14 11:59:42 -0400

    [refactorKeyDispatcher] separated the on and off tests in different its

--- test/dispatchers/keyDispatcherTests.ts
+++ test/dispatchers/keyDispatcherTests.ts
@@ -15,7 +15,7 @@ describe("Dispatchers", () => {
         body = d3.select("body");
       });
 
-      it("calls the onKeydown callback", () => {
+      it("calls the keydown callback", () => {
         let keyDowned = false;
         let callback = (code: number, event: KeyboardEvent) => {
           keyDowned = true;
@@ -28,14 +28,26 @@ describe("Dispatchers", () => {
         TestMethods.triggerFakeKeyboardEvent("keydown", body, keyCodeToSend);
         assert.isTrue(keyDowned, "callback when a key was pressed");
 
-        assert.strictEqual(keyDispatcher.offKeyDown(callback), keyDispatcher, "unsetting the keyDown callback returns the dispatcher");
-        keyDowned = false;
+        keyDispatcher.offKeyDown(callback);
+      });
 
+      it("can remove keydown listener", () => {
+        let keyDowned = false;
+        let callback = () => keyDowned = true;
+
+        assert.strictEqual(keyDispatcher.onKeyDown(callback), keyDispatcher, "setting the keyDown callback returns the dispatcher");
+
+        TestMethods.triggerFakeKeyboardEvent("keydown", body, keyCodeToSend);
+        assert.isTrue(keyDowned, "callback when a key was pressed");
+
+        assert.strictEqual(keyDispatcher.offKeyDown(callback), keyDispatcher, "unsetting the keyDown callback returns the dispatcher");
+
+        keyDowned = false;
         TestMethods.triggerFakeKeyboardEvent("keydown", body, keyCodeToSend);
         assert.isFalse(keyDowned, "nothing happens when a key was pressed");
       });
 
-      it("calls the onKeyup callback", () => {
+      it("calls the keyup callback", () => {
         let keyUped = false;
         let callback = (code: number, event: KeyboardEvent) => {
           keyUped = true;
@@ -46,13 +58,25 @@ describe("Dispatchers", () => {
         assert.strictEqual(keyDispatcher.onKeyUp(callback), keyDispatcher, "setting the keyUp callback returns the dispatcher");
 
         TestMethods.triggerFakeKeyboardEvent("keyup", body, keyCodeToSend);
-        assert.isTrue(keyUped, "callback when a key was release");
+        assert.isTrue(keyUped, "callback when a key was released");
 
-        assert.strictEqual(keyDispatcher.offKeyUp(callback), keyDispatcher, "unsetting the keyUp callback returns the dispatcher");
-        keyUped = false;
+        keyDispatcher.offKeyUp(callback);
+      });
+
+      it("can remove keyup listener", () => {
+        let keyUped = false;
+        let callback = () => keyUped = true;
+
+        keyDispatcher.onKeyUp(callback);
 
         TestMethods.triggerFakeKeyboardEvent("keyup", body, keyCodeToSend);
-        assert.isFalse(keyUped, "nothing happens when a key was release");
+        assert.isTrue(keyUped, "callback when a key was released");
+
+        assert.strictEqual(keyDispatcher.offKeyUp(callback), keyDispatcher, "unsetting the keyup callback returns the dispatcher");
+
+        keyUped = false;
+        TestMethods.triggerFakeKeyboardEvent("keyup", body, keyCodeToSend);
+        assert.isFalse(keyUped, "nothing happens when a key was released");
       });
     });
 
