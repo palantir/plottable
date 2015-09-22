b4ea510020c92499c0de2a3a3205dad61b58741b
Brian Lewis <brian@softwords.com.au>
2015-09-22 21:01:27 +0200

    Key function in Dataset

--- src/core/dataset.ts
+++ src/core/dataset.ts
@@ -2,93 +2,120 @@
 
 module Plottable {
 
-export type DatasetCallback = (dataset: Dataset) => void;
+  export type DatasetCallback = (dataset: Dataset) => void;
 
-export class Dataset {
-  private _data: any[];
-  private _metadata: any;
-  private _callbacks: Utils.CallbackSet<DatasetCallback>;
 
-  /**
-   * A Dataset contains an array of data and some metadata.
-   * Changes to the data or metadata will cause anything subscribed to the Dataset to update.
-   *
-   * @constructor
-   * @param {any[]} [data=[]] The data for this Dataset.
-   * @param {any} [metadata={}] An object containing additional information.
-   */
-  constructor(data: any[] = [], metadata: any = {}) {
-    this._data = data;
-    this._metadata = metadata;
-    this._callbacks = new Utils.CallbackSet<DatasetCallback>();
+  export class KeyFunctions {
+    protected static counter: number = 0;
+    public static NoConstancy: (d: any, i: number) => any = (d: any) => { return KeyFunctions.counter++; };
+    public static ByIndex: (d: any, i: number) => any = (d: any, i: number) => { return i; };
   }
 
-  /**
-   * Adds a callback to be called when the Dataset updates.
-   *
-   * @param {DatasetCallback} callback.
-   * @returns {Dataset} The calling Dataset.
-   */
-  public onUpdate(callback: DatasetCallback) {
-    this._callbacks.add(callback);
-    return this;
-  }
 
-  /**
-   * Removes a callback that would be called when the Dataset updates.
-   *
-   * @param {DatasetCallback} callback
-   * @returns {Dataset} The calling Dataset.
-   */
-  public offUpdate(callback: DatasetCallback) {
-    this._callbacks.delete(callback);
-    return this;
-  }
+  export class Dataset {
+    private _data: any[];
+    private _metadata: any;
+    private _key: (datum: any, index: number) => any = KeyFunctions.NoConstancy;
+    private _callbacks: Utils.CallbackSet<DatasetCallback>;
 
-  /**
-   * Gets the data.
-   *
-   * @returns {any[]}
-   */
-  public data(): any[];
-  /**
-   * Sets the data.
-   *
-   * @param {any[]} data
-   * @returns {Dataset} The calling Dataset.
-   */
-  public data(data: any[]): Dataset;
-  public data(data?: any[]): any {
-    if (data == null) {
-      return this._data;
-    } else {
+    /**
+     * A Dataset contains an array of data and some metadata.
+     * Changes to the data or metadata will cause anything subscribed to the Dataset to update.
+     *
+     * @constructor
+     * @param {any[]} [data=[]] The data for this Dataset.
+     * @param {any} [metadata={}] An object containing additional information.
+     */
+    constructor(data: any[] = [], metadata: any = {}) {
       this._data = data;
-      this._callbacks.callCallbacks(this);
-      return this;
-    }
-  }
-
-  /**
-   * Gets the metadata.
-   *
-   * @returns {any}
-   */
-  public metadata(): any;
-  /**
-   * Sets the metadata.
-   *
-   * @param {any} metadata
-   * @returns {Dataset} The calling Dataset.
-   */
-  public metadata(metadata: any): Dataset;
-  public metadata(metadata?: any): any {
-    if (metadata == null) {
-      return this._metadata;
-    } else {
       this._metadata = metadata;
-      this._callbacks.callCallbacks(this);
+      this._callbacks = new Utils.CallbackSet<DatasetCallback>();
+    }
+
+    /**
+     * Adds a callback to be called when the Dataset updates.
+     *
+     * @param {DatasetCallback} callback.
+     * @returns {Dataset} The calling Dataset.
+     */
+    public onUpdate(callback: DatasetCallback) {
+      this._callbacks.add(callback);
       return this;
     }
+
+    /**
+     * Removes a callback that would be called when the Dataset updates.
+     *
+     * @param {DatasetCallback} callback
+     * @returns {Dataset} The calling Dataset.
+     */
+    public offUpdate(callback: DatasetCallback) {
+      this._callbacks.delete(callback);
+      return this;
+    }
+
+    /**
+     * Gets the data.
+     *
+     * @returns {any[]}
+     */
+    public data(): any[];
+    /**
+     * Sets the data.
+     *
+     * @param {any[]} data
+     * @returns {Dataset} The calling Dataset.
+     */
+    public data(data: any[]): Dataset;
+    public data(data?: any[]): any {
+      if (data == null) {
+        return this._data;
+      } else {
+        this._data = data;
+        this._callbacks.callCallbacks(this);
+        return this;
+      }
+    }
+
+    /**
+     * Gets the metadata.
+     *
+     * @returns {any}
+     */
+    public metadata(): any;
+    /**
+     * Sets the metadata.
+     *
+     * @param {any} metadata
+     * @returns {Dataset} The calling Dataset.
+     */
+    public metadata(metadata: any): Dataset;
+    public metadata(metadata?: any): any {
+      if (metadata == null) {
+        return this._metadata;
+      } else {
+        this._metadata = metadata;
+        this._callbacks.callCallbacks(this);
+        return this;
+      }
+    }
+
+    public key(): (datum: any, index: number) => any;
+    /**
+     * Sets the key.
+     *
+     * @param {any} key
+     * @returns {Dataset} The calling Dataset.
+     */
+    public key(key: (datum: any, index: number) => any): Dataset;
+    public key(key?: (datum: any, index: number) => any): any {
+      if (key == null) {
+        return this._key;
+      } else {
+        this._key = key;
+        this._callbacks.callCallbacks(this);
+        return this;
+      }
+    }
   }
-}
 }
