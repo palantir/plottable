03bfc29b03593ad839ccdf230fb212c9556bc710
Brian Lewis <brian@softwords.com.au>
2015-10-10 19:25:32 +1100

    new animators

---
+++ src/animators/AttrAnimator.ts
@@ -0,0 +1,50 @@
+﻿///<reference path="../reference.ts" />
+module Plottable {
+  export module Animators {
+
+    /**
+     * Base for animators that animate specific attributes, such as Opacity, height... .
+     */
+    export class Attr extends Base implements Animator {
+
+      private _startAttrs: AttributeToAppliedProjector;
+      private _end: AttributeToAppliedProjector;
+
+      public animate(selection: d3.Selection<any>, attrToAppliedProjector: AttributeToAppliedProjector, drawingTarget?: Drawers.DrawingTarget): d3.Selection<any> | d3.Transition<any> {
+
+        // first make the attr to apply to the enter selection before transition
+        // this is attrToAppliedProjector + _start
+        let startProjector: AttributeToAppliedProjector = this.mergeAttrs(attrToAppliedProjector, this.startAttrs());
+        drawingTarget.enter = this.getTransition(drawingTarget.enter, 0)
+          .attr(startProjector);
+
+        drawingTarget.exit = this.getTransition(drawingTarget.exit, this.stepDuration(), this.delay(drawingTarget.exit))
+          .attr(startProjector);
+
+        // now we can call the base class which will append the remove() to the end of the exit transition
+        return super.animate(selection, attrToAppliedProjector, drawingTarget);
+      }
+      /**
+      * Gets the attributes for entering elements. These are overlaid over 
+      * the target atr and 
+      * @returns {number} The current start delay.
+      */
+      public startAttrs(): AttributeToAppliedProjector;
+      /**
+       * Sets the attributes for entering elements. T
+       *
+       * @param {startAttrs} startDelay The start delay in milliseconds.
+       * @returns {Attr} The calling Attr Animator.
+       */
+      public startAttrs(startAttrs: AttributeToAppliedProjector): Attr;
+      public startAttrs(startAttrs?: AttributeToAppliedProjector): any {
+        if (startAttrs == null) {
+          return this._startAttrs;
+        } else {
+          this._startAttrs = startAttrs;
+          return this;
+        }
+      }
+    }
+  }
+}
---
+++ src/animators/baseAnimator.ts
@@ -0,0 +1,257 @@
+﻿///<reference path="../reference.ts" />
+
+module Plottable {
+    export module Animators {
+
+        /**
+         * An Animator with easing and configurable durations and delays.
+         */
+        export class Base implements Animator {
+            /**
+             * The default starting delay of the animation in milliseconds
+             */
+            private static _DEFAULT_START_DELAY_MILLISECONDS = 0;
+            /**
+             * The default duration of one animation step in milliseconds
+             */
+            private static _DEFAULT_STEP_DURATION_MILLISECONDS = 300;
+            /**
+             * The default maximum start delay between each step of an animation
+             */
+            private static _DEFAULT_ITERATIVE_DELAY_MILLISECONDS = 15;
+            /**
+             * The default maximum total animation duration
+             */
+            private static _DEFAULT_MAX_TOTAL_DURATION_MILLISECONDS = Infinity;
+            /**
+             * The default easing of the animation
+             */
+            private static _DEFAULT_EASING_MODE = "exp-out";
+
+            private _startDelay: number;
+            private _stepDuration: number;
+            private _stepDelay: number;
+            private _maxTotalDuration: number;
+            private _easingMode: string;
+
+            /**
+             * Constructs the default animator
+             *
+             * @constructor
+             */
+            constructor() {
+                this._startDelay = Base._DEFAULT_START_DELAY_MILLISECONDS;
+                this._stepDuration = Base._DEFAULT_STEP_DURATION_MILLISECONDS;
+                this._stepDelay = Base._DEFAULT_ITERATIVE_DELAY_MILLISECONDS;
+                this._maxTotalDuration = Base._DEFAULT_MAX_TOTAL_DURATION_MILLISECONDS;
+                this._easingMode = Base._DEFAULT_EASING_MODE;
+            }
+
+            public totalTime(numberOfSteps: number) {
+              let adjustedIterativeDelay = this._getAdjustedIterativeDelay(numberOfSteps);
+              return this.startDelay() + adjustedIterativeDelay * (Math.max(numberOfSteps - 1, 0)) + this.stepDuration();
+            }
+
+            public animate(selection: d3.Selection<any>, attrToAppliedProjector: AttributeToAppliedProjector, drawingTarget?: Drawers.DrawingTarget): d3.Selection<any> | d3.Transition<any> {
+              let numberOfSteps = selection[0].length;
+              let adjustedIterativeDelay = this._getAdjustedIterativeDelay(numberOfSteps);
+
+              // set all the properties on the merge , save the transition returned
+              drawingTarget.merge = this.getTransition(drawingTarget.merge, this.stepDuration(), (d: any, i: number) => this.startDelay() + adjustedIterativeDelay * i)
+                .attr(attrToAppliedProjector);
+              
+              // remove the exiting elements
+              drawingTarget.exit
+                .remove();
+              return drawingTarget.merge;
+            }
+
+            /**
+             * return a transition from the selection, with the requested duration 
+             * and (possibly) delay. As a convenience, this may return the selection itself
+             * without applying a transition if durection = 0
+             *
+             */
+            protected getTransition(selection: d3.Selection<any>|d3.Transition<any>|d3.selection.Update<any>, duration: number, delay?: (d: any, i: number) => number): any {
+                // if the duration is 0, just return the selection
+                if (duration === 0)
+                return selection;
+
+                
+                let selectionIsTransition: boolean = false;
+
+                if (selectionIsTransition) {
+                // if the selection is already a transition, create a new transition, but let d3 supply the default
+                // delay, that way they will "chain"
+                  return selection.transition()
+                    .ease(this.easingMode())
+                    .duration(duration);
+
+                } else {
+                // otherwise is the selection is NOT a transition, create a new transition setting up the delay
+                  return selection.transition()
+                    .ease(this.easingMode())
+                    .duration(duration)
+                    .delay(delay);
+
+                }
+            }
+
+            /**
+             *  return the combined AtributeToAppliedProjector
+             *
+             */
+            protected mergeAttrs(attr1: AttributeToAppliedProjector, attr2: AttributeToAppliedProjector): AttributeToAppliedProjector {
+              for (var attrName in attr2) {
+                attr1[attrName] = attr2[attrName];
+              }
+              return attr1;
+            }
+
+            /**
+             *  return the AtributeToAppliedProjector comprising only those attributes listed in names
+             *
+             */
+            protected pluckAttrs(attr: AttributeToAppliedProjector, names: string[]): AttributeToAppliedProjector {
+              //let out = 
+              return attr;
+            }
+
+            protected delay(selection: any): (d: any, i: number) => number {
+              let dom: any[] = selection[0];
+              let numberOfSteps: number = selection[0].length;
+              let adjustedIterativeDelay: number = this._getAdjustedIterativeDelay(numberOfSteps);
+
+              return (d: any, i: number) => this.startDelay() + adjustedIterativeDelay * i;
+            }
+         
+            /**
+             * Gets the start delay of the animation in milliseconds.
+             *
+             * @returns {number} The current start delay.
+             */
+            public startDelay(): number;
+            /**
+             * Sets the start delay of the animation in milliseconds.
+             *
+             * @param {number} startDelay The start delay in milliseconds.
+             * @returns {Easing} The calling Easing Animator.
+             */
+            public startDelay(startDelay: number): Easing;
+            public startDelay(startDelay?: number): any {
+                if (startDelay == null) {
+                    return this._startDelay;
+                } else {
+                    this._startDelay = startDelay;
+                    return this;
+                }
+            }
+
+            /**
+             * Gets the duration of one animation step in milliseconds.
+             *
+             * @returns {number} The current duration.
+             */
+            public stepDuration(): number;
+            /**
+             * Sets the duration of one animation step in milliseconds.
+             *
+             * @param {number} stepDuration The duration in milliseconds.
+             * @returns {Easing} The calling Easing Animator.
+             */
+            public stepDuration(stepDuration: number): Easing;
+            public stepDuration(stepDuration?: number): any {
+                if (stepDuration == null) {
+                    return Math.min(this._stepDuration, this._maxTotalDuration);
+                } else {
+                    this._stepDuration = stepDuration;
+                    return this;
+                }
+            }
+
+            /**
+             * Gets the maximum start delay between animation steps in milliseconds.
+             *
+             * @returns {number} The current maximum iterative delay.
+             */
+            public stepDelay(): number;
+            /**
+             * Sets the maximum start delay between animation steps in milliseconds.
+             *
+             * @param {number} stepDelay The maximum iterative delay in milliseconds.
+             * @returns {Easing} The calling Easing Animator.
+             */
+            public stepDelay(stepDelay: number): Easing;
+            public stepDelay(stepDelay?: number): any {
+                if (stepDelay == null) {
+                    return this._stepDelay;
+                } else {
+                    this._stepDelay = stepDelay;
+                    return this;
+                }
+            }
+
+            /**
+             * Gets the maximum total animation duration constraint in milliseconds.
+             *
+             * If the animation time would exceed the specified time, the duration of each step
+             * and the delay between each step will be reduced until the animation fits within
+             * the specified time.
+             * 
+             * @returns {number} The current maximum total animation duration.
+             */
+            public maxTotalDuration(): number;
+            /**
+             * Sets the maximum total animation duration constraint in miliseconds.
+             * 
+             * If the animation time would exceed the specified time, the duration of each step
+             * and the delay between each step will be reduced until the animation fits within
+             * the specified time.
+             *
+             * @param {number} maxTotalDuration The maximum total animation duration in milliseconds.
+             * @returns {Easing} The calling Easing Animator.
+             */
+            public maxTotalDuration(maxTotalDuration: number): Easing;
+            public maxTotalDuration(maxTotalDuration?: number): any {
+                if (maxTotalDuration == null) {
+                    return this._maxTotalDuration;
+                } else {
+                    this._maxTotalDuration = maxTotalDuration;
+                    return this;
+                }
+            }
+
+            /**
+             * Gets the current easing mode of the animation.
+             *
+             * @returns {string} the current easing mode.
+             */
+            public easingMode(): string;
+            /**
+             * Sets the easing mode of the animation.
+             *
+             * @param {string} easingMode The desired easing mode.
+             * @returns {Easing} The calling Easing Animator.
+             */
+            public easingMode(easingMode: string): Easing;
+            public easingMode(easingMode?: string): any {
+                if (easingMode == null) {
+                    return this._easingMode;
+                } else {
+                    this._easingMode = easingMode;
+                    return this;
+                }
+            }
+
+            /**
+             * Adjust the iterative delay, such that it takes into account the maxTotalDuration constraint
+             */
+            private _getAdjustedIterativeDelay(numberOfSteps: number) {
+                let stepStartTimeInterval = this.maxTotalDuration() - this.stepDuration();
+                stepStartTimeInterval = Math.max(stepStartTimeInterval, 0);
+                let maxPossibleIterativeDelay = stepStartTimeInterval / Math.max(numberOfSteps - 1, 1);
+                return Math.min(this.stepDelay(), maxPossibleIterativeDelay);
+            }
+        }
+    }
+}
