a4f8d9b2820e9758994802c8b93810be73b85a06
Justin Lan <jlan@palantir.com>
2015-10-22 05:09:20 -0700

    [Utils.Window tests] Add tests for warn()
    Kind of spooky tests that replace console.warn and console.log temporarily.
    Not sure if we should keep these.

--- test/utils/windowUtilsTests.ts
+++ test/utils/windowUtilsTests.ts
@@ -1,8 +1,50 @@
 ///<reference path="../testReference.ts" />
 
 describe("Utils.Window", () => {
-  describe("deprecated()", () => {
+  describe("warn()", () => {
+    it("uses console.warn if available, console.log otherwise", () => {
+      const originalWindowWarn = console.warn;
+      let receivedWarning: string = null;
+      const replacementWarn = (warning: string) => receivedWarning = warning;
+      console.warn = replacementWarn;
 
+      const expectedWarning = "BEEP BOOP";
+      Plottable.Utils.Window.warn(expectedWarning);
+      console.warn = originalWindowWarn;
+      assert.strictEqual(receivedWarning, expectedWarning, "console.warn was passed the expected message");
+
+      const originalWindowLog = console.log;
+      let receivedMessage: string = null;
+      const replacementLog = (warning: string) => receivedMessage = warning;
+      console.warn = null;
+      console.log = replacementLog;
+      receivedWarning = null;
+
+      Plottable.Utils.Window.warn(expectedWarning);
+      console.warn = originalWindowWarn;
+      console.log = originalWindowLog;
+
+      assert.isNull(receivedWarning, "console.warn was not invoked if it doesn't exist");
+      assert.strictEqual(receivedMessage, expectedWarning, "used console.log as a fallback");
+    });
+
+    it("does not emit warnings if Configs.SHOW_WARNINGS is set to false", () => {
+      const originalWindowWarn = console.warn;
+      let windowWarnWasCalled = false;
+      const replacementWarn = (warning: string) => windowWarnWasCalled = true;
+      console.warn = replacementWarn;
+
+      const originalShowWarnings = Plottable.Configs.SHOW_WARNINGS;
+      Plottable.Configs.SHOW_WARNINGS = false;
+      Plottable.Utils.Window.warn("BEEP BOOP");
+      console.warn = originalWindowWarn;
+      Plottable.Configs.SHOW_WARNINGS = originalShowWarnings;
+
+      assert.isFalse(windowWarnWasCalled, "no warning was logged if SHOW_WARNINGS was set to false");
+    });
+  });
+
+  describe("deprecated()", () => {
     let oldWarn: (warning: string) => void;
 
     before(() => {
