b37470ae8d51b3c1f03c50456c2be2fcedcedf5e
Justin Lan <jlan@palantir.com>
2015-11-16 00:32:44 -0800

    [Scale tests] Add one more level of nesting for Scale/QuantitativeScale

--- test/scales/scaleTests.ts
+++ test/scales/scaleTests.ts
@@ -1,44 +1,45 @@
 ///<reference path="../testReference.ts" />
 
 describe("Scales", () => {
-  it("Scale alerts listeners when its domain is updated", () => {
-    let scale = new Plottable.Scale();
-    (<any> scale)._d3Scale = d3.scale.identity();
+  describe("Scale", () => {
+    it("Scale alerts listeners when its domain is updated", () => {
+      let scale = new Plottable.Scale();
+      (<any> scale)._d3Scale = d3.scale.identity();
 
-    let callbackWasCalled = false;
-    let testCallback = (listenable: Plottable.Scale<any, any>) => {
-      assert.strictEqual(listenable, scale, "Callback received the calling scale as the first argument");
-      callbackWasCalled = true;
-    };
-    scale.onUpdate(testCallback);
-    (<any> scale)._setBackingScaleDomain = () => { return; };
-    scale.domain([0, 10]);
-    assert.isTrue(callbackWasCalled, "The registered callback was called");
+      let callbackWasCalled = false;
+      let testCallback = (listenable: Plottable.Scale<any, any>) => {
+        assert.strictEqual(listenable, scale, "Callback received the calling scale as the first argument");
+        callbackWasCalled = true;
+      };
+      scale.onUpdate(testCallback);
+      (<any> scale)._setBackingScaleDomain = () => { return; };
+      scale.domain([0, 10]);
+      assert.isTrue(callbackWasCalled, "The registered callback was called");
+    });
+
+    it("Scale update listeners can be turned off", () => {
+      let scale = new Plottable.Scale();
+      (<any> scale)._d3Scale = d3.scale.identity();
+      (<any> scale)._setBackingScaleDomain = () => { return; };
+
+      let callbackWasCalled = false;
+      let testCallback = (listenable: Plottable.Scale<any, any>) => {
+        assert.strictEqual(listenable, scale, "Callback received the calling scale as the first argument");
+        callbackWasCalled = true;
+      };
+      scale.onUpdate(testCallback);
+      scale.domain([0, 10]);
+      assert.isTrue(callbackWasCalled, "The registered callback was called");
+
+      callbackWasCalled = false;
+      scale.offUpdate(testCallback);
+      scale.domain([11, 19]);
+      assert.isFalse(callbackWasCalled, "The registered callback was not called because the callback was removed");
+    });
   });
 
-  it("Scale update listeners can be turned off", () => {
-    let scale = new Plottable.Scale();
-    (<any> scale)._d3Scale = d3.scale.identity();
-    (<any> scale)._setBackingScaleDomain = () => { return; };
-
-    let callbackWasCalled = false;
-    let testCallback = (listenable: Plottable.Scale<any, any>) => {
-      assert.strictEqual(listenable, scale, "Callback received the calling scale as the first argument");
-      callbackWasCalled = true;
-    };
-    scale.onUpdate(testCallback);
-    scale.domain([0, 10]);
-    assert.isTrue(callbackWasCalled, "The registered callback was called");
-
-    callbackWasCalled = false;
-    scale.offUpdate(testCallback);
-    scale.domain([11, 19]);
-    assert.isFalse(callbackWasCalled, "The registered callback was not called because the callback was removed");
-  });
-
-  describe("extent calculation", () => {
-
-    it("quantitaveScale gives the minimum and maxiumum when the domain is stringy", () => {
+  describe("QuantitativeScale", () => {
+    it("QuantitativeScale gives the minimum and maxiumum when the domain is stringy", () => {
       let values = ["1", "3", "2", "1"];
       let scale = new Plottable.QuantitativeScale();
       let computedExtent = scale.extentOfValues(values);
@@ -46,13 +47,12 @@ describe("Scales", () => {
       assert.deepEqual(computedExtent, ["1", "3"], "the extent is the miminum and the maximum value in the domain");
     });
 
-    it("quantitaveScale gives the minimum and maxiumum when the domain is numeric", () => {
+    it("QuantitativeScale gives the minimum and maxiumum when the domain is numeric", () => {
       let values = [1, 3, 2, 1];
       let scale = new Plottable.QuantitativeScale();
       let computedExtent = scale.extentOfValues(values);
 
       assert.deepEqual(computedExtent, [1, 3], "the extent is the miminum and the maximum value in the domain");
     });
-
   });
 });
