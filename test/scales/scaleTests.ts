55b270b18fbba595c0da55e96a33890b93078264
Justin Lan <jlan@palantir.com>
2015-11-16 01:05:15 -0800

    [Scale tests] Nest tests under "onUpdate" describe()

--- test/scales/scaleTests.ts
+++ test/scales/scaleTests.ts
@@ -2,39 +2,39 @@
 
 describe("Scales", () => {
   describe("Scale", () => {
-    it("Scale alerts listeners when its domain is updated", () => {
-      let scale = new Plottable.Scale();
-      (<any> scale)._d3Scale = d3.scale.identity();
+    describe("onUpdate", () => {
+      it("alerts listeners when its domain is updated", () => {
+        const scale = new Plottable.Scale();
+        (<any> scale)._setBackingScaleDomain = () => { return; };
 
-      let callbackWasCalled = false;
-      let testCallback = (listenable: Plottable.Scale<any, any>) => {
-        assert.strictEqual(listenable, scale, "Callback received the calling scale as the first argument");
-        callbackWasCalled = true;
-      };
-      scale.onUpdate(testCallback);
-      (<any> scale)._setBackingScaleDomain = () => { return; };
-      scale.domain([0, 10]);
-      assert.isTrue(callbackWasCalled, "The registered callback was called");
-    });
+        let callbackWasCalled = false;
+        const testCallback = (listenable: Plottable.Scale<any, any>) => {
+          assert.strictEqual(listenable, scale, "Callback received the calling scale as the first argument");
+          callbackWasCalled = true;
+        };
+        scale.onUpdate(testCallback);
+        scale.domain([0, 10]);
+        assert.isTrue(callbackWasCalled, "The registered callback was called");
+      });
 
-    it("Scale update listeners can be turned off", () => {
-      let scale = new Plottable.Scale();
-      (<any> scale)._d3Scale = d3.scale.identity();
-      (<any> scale)._setBackingScaleDomain = () => { return; };
+      it("can disconnect listeners", () => {
+        const scale = new Plottable.Scale();
+        (<any> scale)._setBackingScaleDomain = () => { return; };
 
-      let callbackWasCalled = false;
-      let testCallback = (listenable: Plottable.Scale<any, any>) => {
-        assert.strictEqual(listenable, scale, "Callback received the calling scale as the first argument");
-        callbackWasCalled = true;
-      };
-      scale.onUpdate(testCallback);
-      scale.domain([0, 10]);
-      assert.isTrue(callbackWasCalled, "The registered callback was called");
+        let callbackWasCalled = false;
+        const testCallback = (listenable: Plottable.Scale<any, any>) => {
+          assert.strictEqual(listenable, scale, "Callback received the calling scale as the first argument");
+          callbackWasCalled = true;
+        };
+        scale.onUpdate(testCallback);
+        scale.domain([0, 10]);
+        assert.isTrue(callbackWasCalled, "The registered callback was called");
 
-      callbackWasCalled = false;
-      scale.offUpdate(testCallback);
-      scale.domain([11, 19]);
-      assert.isFalse(callbackWasCalled, "The registered callback was not called because the callback was removed");
+        callbackWasCalled = false;
+        scale.offUpdate(testCallback);
+        scale.domain([11, 19]);
+        assert.isFalse(callbackWasCalled, "The registered callback was not called because the callback was removed");
+      });
     });
   });
 });
