0ceaa11f2f18b30ed1cb00d850033ed26ee54731
Justin Lan <jlan@palantir.com>
2015-11-18 13:09:22 -0800

    [Arc Drawer tests] Add tests for Drawers.Arc and Drawers.ArcOutline

--- test/testReference.ts
+++ test/testReference.ts
@@ -12,6 +12,7 @@
 ///<reference path="testMethods.ts" />
 
 ///<reference path="drawers/drawerTests.ts" />
+///<reference path="drawers/arcDrawerTests.ts" />
 ///<reference path="drawers/areaDrawerTests.ts" />
 ///<reference path="drawers/lineDrawerTests.ts" />
 
