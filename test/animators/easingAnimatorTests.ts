2593ce7bbb6ba7f64aace308f503aeb5633fe0b4
Brian Lewis <brian@softwords.com.au>
2015-11-22 12:10:01 +1100

    post merge fixups - revert drawerTests

--- test/animators/easingAnimatorTests.ts
+++ test/animators/easingAnimatorTests.ts
@@ -2,124 +2,153 @@
 
 describe("Animators", () => {
   describe("EasingAnimator", () => {
-    let animator: Plottable.Animators.Easing;
+    describe("Time computations", () => {
 
-    beforeEach(() => {
-      animator = new Plottable.Animators.Easing();
-    });
+      it("totalTime() defaults", () => {
+        let iterationSteps = 10;
+        let startDelay = 0;
+        let stepDuration = 300;
+        let stepDelay = 15;
+        let expectedTotalTime = startDelay + (iterationSteps - 1) * stepDelay + stepDuration;
 
-    describe("getters and setters", () => {
+        let animator = new Plottable.Animators.Easing();
+        let actualTotalTime = animator.totalTime(iterationSteps);
 
-      it("can get and set the start delay", () => {
-        const adjustedStartDelay = animator.startDelay() + 10;
-        assert.strictEqual(animator.startDelay(adjustedStartDelay), animator, "setter mode returns the Animator");
-        assert.strictEqual(animator.startDelay(), adjustedStartDelay, "retrieved the set value");
+        assert.strictEqual(actualTotalTime, expectedTotalTime,
+          "Formula for calculating total time should work");
       });
 
-      it("can get and set the step duration", () => {
-        const adjustedStepDuration = animator.stepDuration() + 10;
-        assert.strictEqual(animator.stepDuration(adjustedStepDuration), animator, "setter mode returns the Animator");
-        assert.strictEqual(animator.stepDuration(), adjustedStepDuration, "retrieved the set value");
-      });
+      it("totalTime() takes setters into account", () => {
+        let iterationSteps = 17;
+        let startDelay = 135;
+        let stepDuration = 453;
+        let stepDelay = 265;
+        let expectedTotalTime = startDelay + (iterationSteps - 1) * stepDelay + stepDuration;
 
-      it("can get and set the step delay", () => {
-        const adjustedStepDelay = animator.stepDelay() + 10;
-        assert.strictEqual(animator.stepDelay(adjustedStepDelay), animator, "setter mode returns the Animator");
-        assert.strictEqual(animator.stepDelay(), adjustedStepDelay, "retrieved the set value");
-      });
-
-      it("can get and set the max total duration", () => {
-        const setMaxTotalDuration = 10;
-        assert.strictEqual(animator.maxTotalDuration(setMaxTotalDuration), animator, "setter mode returns the Animator");
-        assert.strictEqual(animator.maxTotalDuration(), setMaxTotalDuration, "retrieved the set value");
-      });
-
-      it("can get and set the easing mode", () => {
-        const setEasingMode = "test-easing";
-        assert.strictEqual(animator.easingMode(setEasingMode), animator, "setter mode returns the Animator");
-        assert.strictEqual(animator.easingMode(), setEasingMode, "retrieved the set value");
-      });
-    });
-
-    describe("total time", () => {
-      const NUM_STEPS = 10;
-
-      it("includes start delay in the total time", () => {
-        const startDelay = 123;
+        let animator = new Plottable.Animators.Easing();
         animator.startDelay(startDelay);
-        animator.stepDelay(0);
-        animator.stepDuration(0);
-
-        const expectedTotalTime = startDelay;
-        assert.strictEqual(animator.totalTime(NUM_STEPS), expectedTotalTime, "total time includes start delay (when other values are 0)");
-      });
-
-      it("includes step delay in the total time", () => {
-        animator.startDelay(0);
-        const stepDelay = 123;
-        animator.stepDelay(stepDelay);
-        animator.stepDuration(0);
-
-        const expectedTotalTime = (NUM_STEPS - 1) * stepDelay;
-        assert.strictEqual(animator.totalTime(NUM_STEPS), expectedTotalTime, "total time increased by each delay between steps");
-      });
-
-      it("includes step duration in the total time", () => {
-        animator.startDelay(0);
-        animator.stepDelay(0);
-        const stepDuration = 123;
         animator.stepDuration(stepDuration);
+        animator.stepDelay(stepDelay);
 
-        const expectedTotalTime = stepDuration;
-        assert.strictEqual(animator.totalTime(NUM_STEPS), expectedTotalTime, "total time increased by one step duration");
-      });
-    });
-
-    describe("max total duration", () => {
-      const EPSILON = 0.0000001; // floating-point errors
-
-      const NUM_STEPS = 10;
-      const ORIGINAL_STEP_DELAY = 100;
-      const ORIGINAL_STEP_DURATION = 100;
-      let totalTimeLimit: number;
-
-      beforeEach(() => {
-        animator.stepDelay(ORIGINAL_STEP_DELAY);
-        animator.stepDuration(ORIGINAL_STEP_DURATION);
-        totalTimeLimit = animator.totalTime(NUM_STEPS);
-
-        animator.maxTotalDuration(totalTimeLimit);
+        let actualTotalTime = animator.totalTime(iterationSteps);
+        assert.strictEqual(actualTotalTime, expectedTotalTime,
+          "Setters should work");
       });
 
-      it("restricts the total time when the number of steps increases", () => {
-        const moreStepsTotalTime = animator.totalTime(2 * NUM_STEPS);
-        assert.closeTo(moreStepsTotalTime, totalTimeLimit, EPSILON,
-          "adding more steps does not increase the total time past the limit");
+      it("maxTotalDuration() with many steps", () => {
+        let iterationSteps = 10000;
+        let startDelay = 0;
+        let stepDuration = 100;
+        let stepDelay = 1000;
+        let expectedTotalTime = 2000;
+
+        let animator = new Plottable.Animators.Easing();
+        animator.startDelay(startDelay);
+        animator.stepDuration(stepDuration);
+        animator.stepDelay(stepDelay);
+        animator.maxTotalDuration(expectedTotalTime);
+
+        let actualTotalTime = animator.totalTime(iterationSteps);
+        assert.strictEqual(actualTotalTime, expectedTotalTime,
+          "Animation should not exceed the maxTotalDuration time constraint when presented with lots of iteration steps");
       });
 
-      it("restricts the total time when the step delay increases", () => {
-        animator.stepDelay(2 * ORIGINAL_STEP_DELAY);
-        const longerStepDelayTotalTime = animator.totalTime(NUM_STEPS);
-        assert.closeTo(longerStepDelayTotalTime, totalTimeLimit, EPSILON,
-          "increasing step delay does not increase the total time past the limit");
+      it("maxTotalDuration() long steps", () => {
+        let iterationSteps = 2;
+        let startDelay = 0;
+        let stepDuration = 10000;
+        let stepDelay = 100;
+        let expectedTotalTime = 2000;
+
+        let animator = new Plottable.Animators.Easing();
+        animator.startDelay(startDelay);
+        animator.stepDuration(stepDuration);
+        animator.stepDelay(stepDelay);
+        animator.maxTotalDuration(expectedTotalTime);
+
+        let actualTotalTime = animator.totalTime(iterationSteps);
+        assert.strictEqual(actualTotalTime, expectedTotalTime,
+          "Animation should not exceed the maxTotalDuration time constraint when step duration set too high");
       });
 
-      it("restricts the total time when the step duration increases", () => {
-        animator.stepDuration(2 * ORIGINAL_STEP_DURATION);
-        const longerStepDurationTotalTime = animator.totalTime(NUM_STEPS);
-        assert.closeTo(longerStepDurationTotalTime, totalTimeLimit, EPSILON,
-          "increasing step duration does not increase the total time past the limit");
+      it("maxTotalDuration() is just a constraint", () => {
+        let iterationSteps = 2;
+        let startDelay = 0;
+        let stepDuration = 100;
+        let stepDelay = 100;
+        let maxTotalDuration = 5000;
+
+        let expectedTotalTime = 200;
+
+        let animator = new Plottable.Animators.Easing();
+        animator.startDelay(startDelay);
+        animator.stepDuration(stepDuration);
+        animator.stepDelay(stepDelay);
+        animator.maxTotalDuration(maxTotalDuration);
+
+        let actualTotalTime = animator.totalTime(iterationSteps);
+        assert.strictEqual(actualTotalTime, expectedTotalTime,
+          "The total duration constraint is just an upper bound");
       });
 
-      it("allows the start delay to increase the total time", () => {
-        const originalTotalTime = animator.totalTime(NUM_STEPS);
+      it("maxTotalDuration() edge case for 1 step", () => {
+        let iterationSteps = 1;
+        let startDelay = 0;
+        let stepDuration = 333;
+        let stepDelay = 432;
 
-        animator.startDelay(100);
-        const totalTimeWithStartDelay = animator.totalTime(NUM_STEPS);
-        const expectedTotalTime = animator.startDelay() + originalTotalTime;
-        assert.closeTo(totalTimeWithStartDelay, expectedTotalTime, EPSILON,
-          "start delay is added on to the max total duration");
+        let animator = new Plottable.Animators.Easing();
+        animator.startDelay(startDelay);
+        animator.stepDuration(stepDuration);
+        animator.stepDelay(stepDelay);
+
+        let actualTotalTime = animator.totalTime(iterationSteps);
+        assert.strictEqual(actualTotalTime, stepDuration,
+          "The total time is just the time for one step");
       });
+
+      it("maxTotalDuration() edge case for 1 step and startDelay", () => {
+        let iterationSteps = 1;
+        let startDelay = 213;
+        let stepDuration = 333;
+        let stepDelay = 432;
+
+        let expectedTotalTime = startDelay + stepDuration;
+
+        let animator = new Plottable.Animators.Easing();
+        animator.startDelay(startDelay);
+        animator.stepDuration(stepDuration);
+        animator.stepDelay(stepDelay);
+
+        let actualTotalTime = animator.totalTime(iterationSteps);
+        assert.strictEqual(actualTotalTime, expectedTotalTime,
+          "Total time is the time for one step, plus waiting for the start delay");
+      });
+
+      it("_getAdjustedIterativeDelay() works with maxTotalDuration constraint", () => {
+        let iterationSteps = 2;
+        let startDelay = 0;
+        let stepDuration = 1000;
+        let stepDelay = 1000000;
+        let maxTotalDuration = 1500;
+
+        let animator = new Plottable.Animators.Easing();
+        animator.startDelay(startDelay);
+        animator.stepDuration(stepDuration);
+        animator.stepDelay(stepDelay);
+        animator.maxTotalDuration(maxTotalDuration);
+
+        let expectedIterativeDelay = 500;
+        // 1 |  ###########
+        // 2 |       ###########
+        //   +------------------------
+        //   |  |    |    |    |    |
+        //     0.0  0.5  1.0  1.5  2.0
+        let actualIterativeDelay = (<any>animator)._getAdjustedIterativeDelay(iterationSteps);
+        assert.strictEqual(actualIterativeDelay, expectedIterativeDelay,
+          "The total duration constraint is just an upper bound");
+      });
+
     });
   });
 });
