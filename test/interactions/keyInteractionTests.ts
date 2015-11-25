9cb8d92b33b155ca9505da1a9dfbb26cae384c30
Justin Lan <jlan@palantir.com>
2015-11-25 12:57:41 -0800

    [Key Interaction tests] Remove unused options argument in helper function

--- test/interactions/keyInteractionTests.ts
+++ test/interactions/keyInteractionTests.ts
@@ -51,15 +51,11 @@ describe("Interactions", () => {
           }
         });
 
-        function triggerKeyEvent(mouseEvent: string,
-                                 p: Plottable.Point,
-                                 target: d3.Selection<void>,
-                                 keycode: number,
-                                 options?: {[key: string]: any}) {
+        function triggerKeyEvent(mouseEvent: string, p: Plottable.Point, target: d3.Selection<void>, keycode: number) {
           TestMethods.triggerFakeMouseEvent(mouseEvent, target, p.x, p.y);
-          TestMethods.triggerFakeKeyboardEvent("keydown", target, keycode, options);
+          TestMethods.triggerFakeKeyboardEvent("keydown", target, keycode);
           if (event === "KeyRelease") {
-            TestMethods.triggerFakeKeyboardEvent("keyup", target, keycode, options);
+            TestMethods.triggerFakeKeyboardEvent("keyup", target, keycode);
           }
         }
 
