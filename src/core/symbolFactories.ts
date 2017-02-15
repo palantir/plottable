914edaa5284bea87f117409685985099bd872666
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-02-14 19:44:00 -0500

    fix symbolFactories; remove triangleDown; add wye and star
    breaking change: no more triangleUp/triangleDown in symbol factory

--- src/core/symbolFactories.ts
+++ src/core/symbolFactories.ts
@@ -12,25 +12,33 @@ import * as d3 from "d3";
 export type SymbolFactory = (symbolSize: number) => string;
 
 export function circle(): SymbolFactory {
-  return (symbolSize: number) => d3.symbol().type("circle").size(Math.PI * Math.pow(symbolSize / 2, 2))(null);
+  return (symbolSize: number) => d3.symbol().type(d3.symbolCircle).size(Math.PI * Math.pow(symbolSize / 2, 2))(null);
 }
 
 export function square(): SymbolFactory {
-  return (symbolSize: number) => d3.symbol().type("square").size(Math.pow(symbolSize, 2))(null);
+  return (symbolSize: number) => d3.symbol().type(d3.symbolSquare).size(Math.pow(symbolSize, 2))(null);
 }
 
 export function cross(): SymbolFactory {
-  return (symbolSize: number) => d3.symbol().type("cross").size((5 / 9) * Math.pow(symbolSize, 2))(null);
+  return (symbolSize: number) => d3.symbol().type(d3.symbolCross).size((5 / 9) * Math.pow(symbolSize, 2))(null);
 }
 
 export function diamond(): SymbolFactory {
-  return (symbolSize: number) => d3.symbol().type("diamond").size(Math.tan(Math.PI / 6) * Math.pow(symbolSize, 2) / 2)(null);
+  return (symbolSize: number) => d3.symbol().type(d3.symbolDiamond).size(Math.tan(Math.PI / 6) * Math.pow(symbolSize, 2) / 2)(null);
 }
 
-export function triangleUp(): SymbolFactory {
-  return (symbolSize: number) => d3.symbol().type("triangle-up").size(Math.sqrt(3) * Math.pow(symbolSize / 2, 2))(null);
+export function triangle(): SymbolFactory {
+  return (symbolSize: number) => d3.symbol().type(d3.symbolTriangle).size(Math.sqrt(3) * Math.pow(symbolSize / 2, 2))(null);
 }
 
-export function triangleDown(): SymbolFactory {
-  return (symbolSize: number) => d3.symbol().type("triangle-down").size(Math.sqrt(3) * Math.pow(symbolSize / 2, 2))(null);
+// copied from https://github.com/d3/d3-shape/blob/e2e57722004acba754ed9edff020282682450c5c/src/symbol/star.js#L3
+const ka = 0.89081309152928522810;
+export function star(): SymbolFactory {
+  return (symbolSize: number) => d3.symbol().type(d3.symbolStar).size(ka * Math.pow(symbolSize / 2, 2))(null);
+}
+
+// copied from https://github.com/d3/d3-shape/blob/c35b2303eb4836aba3171642f01c2653e4228b9c/src/symbol/wye.js#L2
+const a = ((1 / Math.sqrt(12)) / 2 + 1) * 3;
+export function wye(): SymbolFactory {
+  return (symbolSize: number) => d3.symbol().type(d3.symbolWye).size(a * Math.pow(symbolSize / 2, 2))(null);
 }
