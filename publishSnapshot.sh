502fc3279e09d5fc0b4c1ab0257e1f61074e1f61
Benjamin Lee <benjaminl@palantir.com>
2017-04-19 14:23:09 -0400

    update circle.yml and publishSnapshot for new tagging process

--- publishSnapshot.sh
+++ publishSnapshot.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-GIT_DESCRIBE=`git describe --tags`
-
-if [ `git describe --tags --abbrev=0` == $GIT_DESCRIBE ]
-then
-  echo "Cannot snapshot publish concrete version ${GIT_DESCRIBE}"
-  exit 1
-else
-  # set package.json version
-  npm --no-git-tag-version version $GIT_DESCRIBE
-  yarn dist
-  npm publish --tag next
-fi
+GIT_DESCRIBE=`git describe --tags --match "*-dev"`
+# set package.json version
+npm --no-git-tag-version version $GIT_DESCRIBE
+yarn dist
+npm publish --tag next
