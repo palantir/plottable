29ddee3d0941aaf008011b24224dec011ec93e9f
Xiaohan Zhang <hellochar@users.noreply.github.com>
2017-06-09 17:08:17 -0700

    allow null or undefined type

--- src/components/table.ts
+++ src/components/table.ts
@@ -50,10 +50,10 @@ export class Table extends ComponentContainer {
    * ```
    *
    * @constructor
-   * @param {(Component|null)[][]} [rows=[]] A 2-D array of Components to be added to the Table.
+   * @param {(Component|null|undefined)[][]} [rows=[]] A 2-D array of Components to be added to the Table.
    *   null can be used if a cell is empty.
    */
-  constructor(rows: (Component|null)[][] = []) {
+  constructor(rows: (Component|null|undefined)[][] = []) {
     super();
     this.addClass("table");
     rows.forEach((row, rowIndex) => {
