24ff4b9c838af28f6e30c3c924d643469177e5a9
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-06-27 17:21:48 -0700

    fix tests

--- test/utils/transformAwareTranslatorTests.ts
+++ test/utils/transformAwareTranslatorTests.ts
@@ -8,58 +8,32 @@ import * as TestMethods from "../testMethods";
 
 describe("Translator", () => {
     it("getTranslator() creates only one Translator per Component", () => {
-        const div = TestMethods.generateDiv();
+        const svg = TestMethods.generateSVG();
         const component = new Plottable.Component();
+        sinon.stub(component, "content").returns(svg);
 
         const t1 = Plottable.Utils.getTranslator(component);
         assert.isNotNull(t1, "created a new Translator on a Component");
         const t2 = Plottable.Utils.getTranslator(component);
         assert.strictEqual(t1, t2, "returned the existing Translator if called again with same Component");
 
-        div.remove();
-      });
+        svg.remove();
+    });
 
-    it("converts points to html-space correctly", () => {
-        const div = TestMethods.generateDiv();
-        div.style("position", "relative");
-        const component = new Plottable.Component();
-        sinon.stub(component, "rootElement").returns(div);
-
-        const divOrigin: Plottable.Point = {
-          x: 19,
-          y: 85,
-        };
-        const rect = div.append("div").styles({
-          left: `${divOrigin.x}px`,
-          position: "absolute",
-          top: `${divOrigin.y}px`,
-          width: "30px",
-          height: "30px",
-        });
-
-        const translator = Plottable.Utils.getTranslator(component);
-
-        const boundingClientRect = (<Element> rect.node()).getBoundingClientRect();
-        const computedOrigin = translator.computePosition(boundingClientRect.left, boundingClientRect.top);
-        TestMethods.assertPointsClose(computedOrigin, divOrigin, 0.5, "translates client coordinates to <svg> coordinates correctly");
-
-        div.remove();
-      });
-
-    it("converts points to <svg>-space correctly", () => {
+    it("converts points to Component space correctly", () => {
         const svg = TestMethods.generateSVG();
         const component = new Plottable.Component();
-        sinon.stub(component, "rootElement").returns(svg);
+        sinon.stub(component, "content").returns(svg);
 
         const rectOrigin: Plottable.Point = {
-          x: 19,
-          y: 85,
+            x: 19,
+            y: 85,
         };
         const rect = svg.append("rect").attrs({
-          x: rectOrigin.x,
-          y: rectOrigin.y,
-          width: 30,
-          height: 30,
+            x: rectOrigin.x,
+            y: rectOrigin.y,
+            width: 30,
+            height: 30,
         });
 
         const translator = Plottable.Utils.getTranslator(component);
