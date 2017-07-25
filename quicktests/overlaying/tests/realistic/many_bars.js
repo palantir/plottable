960f53ee2ef6450c590b4502efa9bcad87d59b91
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-07-25 15:38:05 -0700

    update bars to 4x1k series

--- quicktests/overlaying/tests/realistic/many_bars.js
+++ quicktests/overlaying/tests/realistic/many_bars.js
@@ -6,7 +6,8 @@ function makeData() {
     var data1 = [];
     var data2 = [];
     var data3 = [];
-    for (var i = 0; i < 3000; i++) {
+    var data4 = [];
+    for (var i = 0; i < 1000; i++) {
         data1.push({
             x: new Date(JAN_01_2012 + MILLIS_IN_DAY * i),
             y: Math.random() * 50 - 25,
@@ -19,8 +20,12 @@ function makeData() {
             x: new Date(JAN_01_2012 + MILLIS_IN_DAY * i),
             y: Math.random() * 50 - 25,
         });
+        data4.push({
+            x: new Date(JAN_01_2012 + MILLIS_IN_DAY * i),
+            y: Math.random() * 50 - 25,
+        });
     }
-    return [data1, data2, data3];
+    return [data1, data2, data3, data4];
 }
 
 function run(div, datas, Plottable) {
@@ -33,8 +38,9 @@ function run(div, datas, Plottable) {
     var ds1 = new Plottable.Dataset(datas[0]).metadata(1);
     var ds2 = new Plottable.Dataset(datas[1]).metadata(2);
     var ds3 = new Plottable.Dataset(datas[2]).metadata(3);
+    var ds4 = new Plottable.Dataset(datas[3]).metadata(4);
     var plot = new Plottable.Plots.StackedBar()
-        .datasets([ds1, ds2, ds3])
+        .datasets([ds1, ds2, ds3, ds4])
         .x(function (d) { return d.x; }, xScale)
         .y(function (d) { return d.y; }, yScale)
         .attr("fill", function (d, i, ds) { return ds.metadata()}, colorScale);
