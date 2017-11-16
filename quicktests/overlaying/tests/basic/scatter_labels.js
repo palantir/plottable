6dd9117c49c4a0d746ad312fc57d755bdb17911c
Charles Meyer <cmeyer@palantir.com>
2017-11-16 12:21:44 -0500

    Add scatterplot label test

---
+++ quicktests/overlaying/tests/basic/scatter_labels.js
@@ -0,0 +1,87 @@
+function makeData() {
+    "use strict";
+
+    var data = [
+        {
+            x: 10,
+            y: 10,
+            label: "these",
+            size: 100
+        },
+        {
+            x: 15,
+            y: 30,
+            label: "Label on bubble",
+            size: 30
+        },
+        {
+            x: 20,
+            y: 40,
+            label: "Label off bubble",
+            size: 10
+        },
+        {
+            x: 20,
+            y: 20,
+            label: "are",
+            size: 150
+        },
+        {
+            x: 30,
+            y: 30,
+            label: "scatter",
+            size: 200
+        },
+        {
+            x: 40,
+            y: 40,
+            label: "plot",
+            size: 300
+        },
+        {
+            x: 50,
+            y: 50,
+            label: "labels",
+            size: 150   
+        }
+    ];
+
+    return data;
+}
+
+function run(div, data, Plottable) {
+    "use strict";
+
+    var xScale = new Plottable.Scales.Linear();
+    var yScale = new Plottable.Scales.Linear();
+    var xAxis = new Plottable.Axes.Numeric(xScale, "bottom");
+    var yAxis = new Plottable.Axes.Numeric(yScale, "left");
+
+    // using the same symbol factories instead of new instances from each
+    // projector allows us to compare the instances and save a lot of re-rendering
+    var symbols = [
+        new Plottable.SymbolFactories.cross(),
+        new Plottable.SymbolFactories.square(),
+        new Plottable.SymbolFactories.star(),
+    ];
+
+    var plot = new Plottable.Plots.Scatter()
+        .renderer("svg")
+        .deferredRendering(true)
+        .addDataset(new Plottable.Dataset(data))
+        .x((d) => d.x, xScale)
+        .y((d) => d.y, yScale)
+        .size((d) => d.size);
+
+    var table = new Plottable.Components.Table([
+        [yAxis, plot],
+        [null, xAxis]
+    ]);
+
+    var panZoom = new Plottable.Interactions.PanZoom(xScale, yScale).attachTo(plot);
+
+    table.renderTo(div);
+
+    panZoom.setMinMaxDomainValuesTo(xScale);
+    panZoom.setMinMaxDomainValuesTo(yScale);
+}
