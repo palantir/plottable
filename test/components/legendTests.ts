b3aff141f6f0cdfc8fc47b2f5e29bf8d4db7c551
Jack <jackb@palantir.com>
2019-12-19 14:10:23 +0000

    Shortened text again due to different text length measures on CircleCI

--- test/components/legendTests.ts
+++ test/components/legendTests.ts
@@ -202,7 +202,7 @@ describe("Legend", () => {
     });
 
     it("wraps entries onto extra rows if necessary for horizontal legends", () => {
-      color.domain(["George Waaaaaaaaaaaaashington", "John Adaaaaaaaaaaaaaams", "Thomaaaaaaaaas Jefferson"]);
+      color.domain(["George Waaaaaaaaashington", "John Adaaaaaaaaaams", "Thomaaaaaaas Jefferson"]);
       legend.maxEntriesPerRow(Infinity);
 
       legend.renderTo(div);
